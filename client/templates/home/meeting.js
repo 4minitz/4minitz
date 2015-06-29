ed933c300c1029e9e830c6f5639a2ca6b82bed95
Mike Lerch <mike.in.flames@gmail.com>
2015-06-29 20:54:00 +0200

    Refactor: hand over meeting id instead of meeting object.

--- client/templates/home/meeting.js
+++ client/templates/home/meeting.js
@@ -1,7 +1,7 @@
 var _meeting;   // the parent meeting of this minutes
 
 Template.meeting.created = function () {
-    _meeting = this.data;
+    _meeting = Meetings.findOne(this.data);
 };
 
 Template.meeting.helpers({
