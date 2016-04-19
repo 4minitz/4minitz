7f53b117e55cb03f2d1befd04bff1ff4dbbbf2b8
Michael Gerhaeuser <michael.gerhaeuser@gmail.com>
2016-04-19 06:15:54 +0200

    Add a fallback redirect in case an error occurs
    In case something goes wrong while adding new minutes,
    the user is now redirected to something that most
    likely exists. Should be extended to redirect the
    user to an error page that explains what happened.
    Or the meetingseries/:_id page with a flash message/
    modal box, that explains what happened.
    
    Important: Really redirect the user, otherwise the
    minutesadd/:_id page will be re-rendered because
    minutesAdd changes the meeting series document.
    Re-rendering potentially causes re-adding of new
    minutes which might trap the user in an endless loop,
    creating numerous new minutes.

--- client/router.js
+++ client/router.js
@@ -17,12 +17,18 @@ Router.route('/minutesadd/:_id', function () {
     let meetingSeriesID = this.params._id;
     ms = new MeetingSeries(meetingSeriesID);
 
-    let id = '';
+    let id;
     ms.addNewMinutes(newMinutesID => {
         id = newMinutesID;
     });
 
-    this.redirect('/minutesedit/' + id);
+    // callback should have been called by now
+    if (id) {
+        this.redirect('/minutesedit/' + id);
+    } else {
+        // todo: use error page
+        this.redirect('/meetingseries/' + meetingSeriesID);
+    }
 });
 
 Router.route('/minutesedit/:_id', function () {
