eff9557c233b7d9ccd2690a13be2fb55132e2b8a
Felix Blechschmitt <fblechschmitt@hiorg-server.de>
2016-04-25 18:26:05 +0200

    Dynamic templates to switch between tabs in meetingSeries-Detail view

--- client/templates/meetingSeriesDetails.js
+++ client/templates/meetingSeriesDetails.js
@@ -1,10 +1,12 @@
 
 import { MeetingSeries } from '/imports/meetingseries'
+import { Minutes } from '/imports/minutes'
 
 var _meetingSeriesID;   // the parent meeting object of this minutes
 
 Template.meetingSeriesDetails.onCreated(function () {
     _meetingSeriesID = this.data;
+    Session.setDefault("currentTab", "minutesList");
 });
 
 Template.meetingSeriesDetails.helpers({
@@ -15,6 +17,26 @@ Template.meetingSeriesDetails.helpers({
     minutes: function() {
         let ms = new MeetingSeries(_meetingSeriesID);
         return ms.getAllMinutes();
+    },
+
+    tab: function() {
+        return Session.get("currentTab");
+    },
+
+    tabData: function() {
+        let tab = Session.get("currentTab");
+        let ms = new MeetingSeries(_meetingSeriesID);
+
+        switch (tab) {
+            case "minutesList":
+                return ms.getAllMinutes();
+
+            case "actionItemList":
+                return Session.get("actionItemStatus");
+
+            case "decisionList":
+                break;
+        }
     }
 });
 
@@ -32,5 +54,17 @@ Template.meetingSeriesDetails.events({
     },
     "click #btnHideHelp": function () {
         $(".help").hide();  // use jQuery to find and hide class
+    },
+    "click .nav-tabs li": function(event, tmpl) {
+        var currentTab = $(event.target).closest("li");
+
+        currentTab.addClass("active");
+        $(".nav-tabs li").not(currentTab).removeClass("active");
+
+        Session.set("currentTab", currentTab.data("template"));
+
+        if (currentTab.data("action")) {
+            Session.set("actionItemStatus", currentTab.data("action"));
+        }
     }
 });
