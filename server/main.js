212033e54f0d4729b0ade59aaaad19074b388cc0
Michael Gerhaeuser <michael.gerhaeuser@gmail.com>
2016-05-09 21:46:22 +0200

    Fix meteor method not found error by initializing meteor methods on the server

--- server/main.js
+++ server/main.js
@@ -1,6 +1,9 @@
 import { Meteor } from 'meteor/meteor';
 import { handleMigration } from './migrations';
 
+import '/imports/minutes';
+import '/imports/meetingseries';
+
 Meteor.startup(() => {
     handleMigration();
 });
