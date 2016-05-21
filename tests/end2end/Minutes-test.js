91d1b5ea7d3cd83935fc859cb3e0a7f42478ee1b
Wolfram Esser <github@derwok.de>
2016-05-21 13:55:17 +0200

    Test: E2E:  tests for minutes "Finalize"
        ✓ can not add minutes if unfinalized minutes exist
        ✓ can finalize not yet finalized minutes

--- tests/end2end/Minutes-test.js
+++ tests/end2end/Minutes-test.js
@@ -9,8 +9,7 @@ describe('Minutes', function () {
     });
 
 
-
-    it('can add first minutes to meeting series @watch', function () {
+    it('can add first minutes to meeting series', function () {
         let aProjectName = "E2E Minutes";
         let aMeetingName = "Meeting Name #1";
 
@@ -23,7 +22,7 @@ describe('Minutes', function () {
     });
 
 
-    it('can add further minutes to meeting series @watch', function () {
+    it('can add further minutes to meeting series', function () {
         let aProjectName = "E2E Minutes";
         let aMeetingName = "Meeting Name #2";
 
@@ -38,7 +37,7 @@ describe('Minutes', function () {
     });
 
 
-    it('can add minutes for specific date @watch', function () {
+    it('can add minutes for specific date', function () {
         let aProjectName = "E2E Minutes";
         let aMeetingName = "Meeting Name #3";
         let myDate = "2015-03-17";
@@ -50,5 +49,48 @@ describe('Minutes', function () {
         e2e.finalizeCurrentMinutes();
         expect(e2e.countMinutesForSeries(aProjectName, aMeetingName)).to.equal(1);
         expect(e2e.getMinutesId(myDate)).to.be.ok;
+    });
+
+
+    it('can not add minutes if unfinalized minutes exist @watch', function () {
+        let aProjectName = "E2E Minutes";
+        let aMeetingName = "Meeting Name #4";
+
+        e2e.createMeetingSeries(aProjectName, aMeetingName);
+        let countInitialMinutes = e2e.countMinutesForSeries(aProjectName, aMeetingName);
+
+        e2e.addMinutesToMeetingSeries(aProjectName, aMeetingName);
+        // No finalize here!
+        e2e.gotoMeetingSeries(aProjectName, aMeetingName);
+        browser.waitForVisible("#btnAddMinutes");
+
+        // check if button is disabled
+        expect(browser.isExisting('a#btnAddMinutes[disabled="true"]')).to.be.true;
+        // check if button is clicked, it does not add minutes
+        browser.click("#btnAddMinutes");
+        expect(e2e.countMinutesForSeries(aProjectName, aMeetingName)).to.equal(countInitialMinutes +1);
+    });
+
+
+    it('can finalize not yet finalized minutes @watch', function () {
+        let aProjectName = "E2E Minutes";
+        let aMeetingName = "Meeting Name #5";
+
+        e2e.createMeetingSeries(aProjectName, aMeetingName);
+        let countInitialMinutes = e2e.countMinutesForSeries(aProjectName, aMeetingName);
+
+        e2e.addMinutesToMeetingSeries(aProjectName, aMeetingName);
+        // No finalize here!
+        e2e.gotoMeetingSeries(aProjectName, aMeetingName);
+        browser.waitForVisible("#btnAddMinutes");
+        browser.click("a#id_linkToMinutes");        // goto first available minutes
+        // Now finalize!
+        e2e.finalizeCurrentMinutes();
+        e2e.gotoMeetingSeries(aProjectName, aMeetingName);
+
+        // check if button is clicked, it does add 2nd minutes
+        browser.click("#btnAddMinutes");
+        e2e.gotoMeetingSeries(aProjectName, aMeetingName);
+        expect(e2e.countMinutesForSeries(aProjectName, aMeetingName)).to.equal(countInitialMinutes +2);
     });
 });
