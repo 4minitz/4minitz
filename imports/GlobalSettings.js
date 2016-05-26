aa584eba88983907385f544b1257c5b470ca95e9
Felix Blechschmitt <fblechschmitt@hiorg-server.de>
2016-05-26 14:48:00 +0200

    Granted easy access to our mail settings via static GlobalSettings class

--- imports/GlobalSettings.js
+++ imports/GlobalSettings.js
@@ -1,7 +1,38 @@
 export class GlobalSettings {
 
     static getDefaultEmailSenderAddress() {
-        return 'minutesgang@gmx.de';
+        if (Meteor.settings.defaultEMailSenderAddress) {
+            return Meteor.settings.defaultEMailSenderAddress;
+        }
+
+        throw new Meteor.Error('illegal-state', 'defaultEMailSenderAddress not defined in settings');
+    }
+
+    static isEMailDeliveryEnabled() {
+        return Meteor.settings.enableMailDelivery;
+    }
+
+    static getMailDeliverer() {
+        if (Meteor.settings.mailDeliverer) {
+            return Meteor.settings.mailDeliverer;
+        }
+
+        return "smtp";
+    }
+
+    static getMailgunSettings() {
+        if (Meteor.Settings.mailgunApiKey
+            && Meteor.Settings.mailgunDomain
+            && Meteor.Settings.mailgunApiUrl
+        ) {
+            return {
+                apiKey: Meteor.Settings.mailgunApiKey,
+                domain: Meteor.Settings.mailgunDomain,
+                apiUrl: Meteor.Settings.mailgunApiUrl
+            };
+        }
+
+        throw new Meteor.Error('illegal-state', 'mailgun settings not defined in meteor settings');
     }
 
 }
