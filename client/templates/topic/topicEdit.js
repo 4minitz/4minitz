86275ee1c45b483e76c0cefe07c272e0985603ba
Wolfram Esser <github@derwok.de>
2016-06-15 09:34:34 +0200

    WIP: select2 on topic responsibles

--- client/templates/topic/topicEdit.js
+++ client/templates/topic/topicEdit.js
@@ -35,34 +35,53 @@ var getEditTopic = function() {
     return new Topic(_minutesID, topicId);
 };
 
+
+var getPossibleResponsibles = function() {
+    let possibleResponsibles = [];          // sorted later on
+    let possibleResponsiblesUnique = {};    // ensure uniqueness
+    let buffer = [];                        // userIds and names from different sources, may have doubles
+
+    // add regular participants from current minutes
+    let aMin = new Minutes(_minutesID);
+    for (let i in aMin.participants) {
+        buffer.push(aMin.participants[i].userId);
+    }
+
+    // add the "additional participants" from current minutes as simple strings
+    let participantsAdditional = aMin.participantsAdditional;
+    if (participantsAdditional) {
+        let splitted = participantsAdditional.split(/[,;]/);
+        for (let i in splitted) {
+            buffer.push(splitted[i].trim());
+        }
+    }
+
+    // add the responsibles from current topic
+    let topic = getEditTopic();
+    if (topic) {
+        buffer.concat(topic._topicDoc.responsibles);
+    }
+
+    for (let i in buffer) {
+        let aResponsibleId = buffer[i];
+        if (! possibleResponsiblesUnique[aResponsibleId]) { // not seen?
+            possibleResponsiblesUnique[aResponsibleId] = true;
+            let aResponsibleName = aResponsibleId;
+            let aUser = Meteor.users.findOne(aResponsibleId);
+            if (aUser) {
+                aResponsibleName = aUser.username;
+            }
+            possibleResponsibles.push({username: aResponsibleName, userId: aResponsibleId});
+        }
+    }
+
+    return possibleResponsibles;
+};
+
 Template.topicEdit.helpers({
     'getTopicSubject': function() {
         let topic = getEditTopic();
         return (topic) ? topic._topicDoc.subject : "";
-    },
-
-    'getTopicResponsible': function() {
-        let topic = getEditTopic();
-        return (topic) ? topic._topicDoc.responsible : "";
-    },
-
-    'getMinutesParticipants'() {
-        let aMin = new Minutes(_minutesID);
-        let participants = aMin.participants;
-        for (let i in participants) {
-            participants[i].username = Meteor.users.findOne(participants[i].userId).username;
-        }
-
-        // add the additional participants, by splitting string up
-        let participantsAdditional = aMin.participantsAdditional;
-        if (participantsAdditional) {
-            let splitted = participantsAdditional.split(/[,;]/);
-            for (let i in splitted) {
-                let aParticipant = splitted[i].trim();
-                participants.push({username: aParticipant, userId: aParticipant})
-            }
-        }
-        return participants;
     }
 });
 
@@ -105,11 +124,39 @@ Template.topicEdit.events({
     },
 
     "show.bs.modal #dlgAddTopic": function (evt, tmpl) {
-        $('#id_selResponsible').val(null).trigger("change");
+        let selectResponsibles = $('#id_selResponsible');
+        selectResponsibles.val(null)        // clear <option>s and selections
+            .find('option')
+            .remove()
+            .end();
+        let possResp = getPossibleResponsibles();
+        for (let i in possResp) {
+            selectResponsibles.append('<option value="'+ possResp[i].userId +'">'+ possResp[i].username +'</option>');
+        }
+
+        let topic = getEditTopic();
+        if (topic && topic._topicDoc && topic._topicDoc.responsibles) {
+            console.log(topic._topicDoc.responsibles);
+            selectResponsibles.val(topic._topicDoc.responsibles);
+        }
+        selectResponsibles.trigger("change");
     },
 
     "shown.bs.modal #dlgAddTopic": function (evt, tmpl) {
         $('#dlgAddTopic input').trigger("change");    // ensure new values trigger placeholder animation
         tmpl.find("#id_subject").focus();
+    },
+
+    "select2:selecting #id_selResponsible"(evt, tmpl) {
+        // console.log(evt);
+        console.log("selecting:"+evt.params.args.data.id + "/"+evt.params.args.data.text);
+        // evt.preventDefault();
+        // return false;
+    },
+
+    "select2:select #id_selResponsible"(evt, tmpl) {
+        // console.log(evt);
+        console.log("select:"+evt.params.data.id + "/"+evt.params.data.text);
     }
+
 });
