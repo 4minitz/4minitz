6ac9473989bd276925e5a5c79013c7324aa3429f
Felix Blechschmitt <fblechschmitt@hiorg-server.de>
2016-06-21 12:24:44 +0200

    WIP: Added labels to meetingseries / infoitem schema (incl. schema migration)
    Added default labels to settings.json additionally.

---
+++ imports/collections/label.schema.js
@@ -0,0 +1,9 @@
+import { SimpleSchema } from 'meteor/aldeed:simple-schema';
+
+export const LabelSchema = new SimpleSchema({
+    _id: {type: String, regEx: SimpleSchema.RegEx.Id},
+    isDefaultLabel: {type: Boolean, defaultValue: false},
+    isDisabled: {type: Boolean, defaultValue: false},
+    name: {type: String},
+    color: {type: String}
+});
