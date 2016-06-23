7dc3b3bee0bc59934a65bf8204a5e914e1c20bf8
Michael Gerhaeuser <michael.gerhaeuser@gmail.com>
2016-06-23 22:25:28 +0200

    Close the meeting series add form via Escape

--- client/templates/meetingSeriesAdd.js
+++ client/templates/meetingSeriesAdd.js
@@ -1,15 +1,31 @@
 import { MeetingSeries } from '/imports/meetingseries';
 import { $ } from 'meteor/jquery';
 
-Template.meetingSeriesAdd.onCreated(function () {
-});
-
 Template.meetingSeriesAdd.onRendered(function () {
     $.material.init();
 });
 
+function clearForm(template) {
+    template.find("#id_meetingproject").value = "";
+    template.find("#id_meetingname").value = "";
+    template.find("#id_meetingproject").focus();
+}
+
+function escapeHandler(event) {
+    event.preventDefault();
+
+    let escapeWasPressed = event.key == 'Escape';
+
+    // for browsers that do not support event.key yet
+    escapeWasPressed |= event.keyCode === 27;
+
+    if (escapeWasPressed) {
+        $('#collapseMeetingSeriesAdd').collapse('hide');
+    }
+}
+
 Template.meetingSeriesAdd.events({
-    "submit #id_meetingSeriesAddForm": function (event, template) {
+    "submit #id_meetingSeriesAddForm"(event, template) {
         event.preventDefault();
 
         var aProject = template.find("#id_meetingproject").value;
@@ -26,16 +42,19 @@ Template.meetingSeriesAdd.events({
                 Session.set('errorTitle', 'Error');
                 Session.set('errorReason', error.reason);
             } else {
-                // Clear form
-                template.find("#id_meetingproject").value = "";
-                template.find("#id_meetingname").value = "";
-                template.find("#id_meetingproject").focus();
+                clearForm(template);
             }
         });
     },
 
-    "shown.bs.collapse #collapseMeetingSeriesAdd": function (evt, tmpl) {
+    "hidden.bs.collapse #collapseMeetingSeriesAdd"(evt, tmpl) {
+        clearForm(tmpl);
+        document.removeEventListener('keyup', escapeHandler);
+    },
+
+    "shown.bs.collapse #collapseMeetingSeriesAdd"(evt, tmpl) {
         tmpl.find("#id_meetingproject").focus();
+        document.addEventListener('keyup', escapeHandler);
     }
 
 });
