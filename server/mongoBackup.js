aeb270094744bd5dabb935075a0c7ee3a9f8874d
Michael Gerhaeuser <michael.gerhaeuser@gmail.com>
2016-06-28 20:59:36 +0200

    Replace bootstrap related packages with npm modules
    This includes bootstrap, bootstrap-datetimepicker and bootstrap-material-design

--- server/mongoBackup.js
+++ server/mongoBackup.js
@@ -1,4 +1,4 @@
-import { moment } from 'meteor/momentjs:moment';
+import moment from 'moment/moment';
 import Future from 'fibers/future';
 import { spawn } from 'child_process';
 import os from 'os';
