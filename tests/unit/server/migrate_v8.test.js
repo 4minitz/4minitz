9ca0acfd0b14f7697dedacb9026269eb61ba8690
Wolfram Esser <github@derwok.de>
2016-07-10 20:30:02 +0200

    Fix: Broken unittests on migrate_v8.test.js

--- tests/unit/server/migrate_v8.test.js
+++ tests/unit/server/migrate_v8.test.js
@@ -3,9 +3,16 @@
 // Its just a quick fix that prohibits a test exception during other migration tests
 
 import { expect } from 'chai';
+import sinon from 'sinon';
 import proxyquire from 'proxyquire';
 
 require('../../../lib/helpers');
+
+class MeteorError {}
+let Meteor = {
+    call: sinon.stub(),
+    Error: MeteorError
+};
 
 let MeetingSeriesCollection = {
 };
@@ -15,6 +22,7 @@ let GlobalSettings = {};
 const {
         MigrateV8
     } = proxyquire('../../../server/migrate_v8', {
+        'meteor/meteor': { Meteor, '@noCallThru': true},
         '/imports/collections/meetingseries_private': { MeetingSeriesCollection, '@noCallThru': true},
         '/imports/GlobalSettings': { GlobalSettings, '@noCallThru': true}
     });
