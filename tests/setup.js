df95df1e7fb42e1fd00d6e4b597c143af867d0fe
Michael Gerhaeuser <michael.gerhaeuser@gmail.com>
2016-07-21 21:38:00 +0200

    Try to make builds more stable
    This introduces a setup script that runs the meteor end2end server and
    kills it as soon as it has started and then exits itself. The advantage
    of this is that we now have a install step that downloads all packages.
    Since it is not started in the background Travis can wait for the initial
    meteor setup before proceeding with the tests.

---
+++ tests/setup.js
@@ -0,0 +1,22 @@
+let child_process = require('child_process');
+
+function capture(err, stdout, stderr) {
+    console.error(err);
+    console.log(stdout);
+    console.warn(stderr);
+}
+
+let meteor = child_process.spawn('npm', ['run', 'test:end2end:server']);
+
+let output = '';
+
+meteor.stdout.on('data', (data) => {
+    let buffer = data.toString();
+    output += buffer;
+    console.log(buffer);
+
+    if (output.indexOf('=> App running at: http://localhost') > -1) {
+        meteor.kill();
+    }
+});
+
