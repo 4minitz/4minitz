75a506491f8f404f9793f04f0c7c7dd3f44da524
Michael Gerhaeuser <michael.gerhaeuser@gmail.com>
2016-07-27 20:09:31 +0200

    Fix and reactivate topic drag&drop tests

--- tests/end2end/Topics-test.js
+++ tests/end2end/Topics-test.js
@@ -1,10 +1,9 @@
-import { E2EGlobal } from './helpers/E2EGlobal'
-import { E2EApp } from './helpers/E2EApp'
-import { E2EMeetingSeries } from './helpers/E2EMeetingSeries'
-import { E2EMeetingSeriesEditor } from './helpers/E2EMeetingSeriesEditor'
-import { E2EMinutes } from './helpers/E2EMinutes'
-import { E2ETopics } from './helpers/E2ETopics'
-
+import { E2EGlobal } from './helpers/E2EGlobal';
+import { E2EApp } from './helpers/E2EApp';
+import { E2EMeetingSeries } from './helpers/E2EMeetingSeries';
+import { E2EMeetingSeriesEditor } from './helpers/E2EMeetingSeriesEditor';
+import { E2EMinutes } from './helpers/E2EMinutes';
+import { E2ETopics } from './helpers/E2ETopics';
 
 describe('Topics', function () {
     const aProjectName = "E2E Topics";
@@ -122,12 +121,17 @@ describe('Topics', function () {
         expect(visibleText).to.have.string('yet another topic');
     });
 
-    xit('can change the order of topics via drag and drop by clicking on the sort icon', function () {
+    it('can change the order of topics via drag and drop by clicking on the sort icon', function () {
         E2ETopics.addTopicToMinutes('some topic');
         E2ETopics.addTopicToMinutes('some other topic');
         E2ETopics.addTopicToMinutes('yet another topic');
 
-        browser.dragAndDrop('#topicPanel .well:nth-child(3) .topicDragDropHandle', '#topicPanel .well:nth-child(1)');
+        browser.waitForExist('#topicPanel .well:nth-child(3) .topicDragDropHandle');
+        browser.moveToObject('#topicPanel .well:nth-child(3) .topicDragDropHandle');
+        browser.buttonDown();
+        browser.moveTo(1, 1);
+        browser.moveToObject('#topicPanel .well:nth-child(1)');
+        browser.buttonUp();
 
         var topics = E2ETopics.getTopicsForMinute();
         let elementId = topics[0].ELEMENT;
@@ -215,12 +219,17 @@ describe('Topics', function () {
     });
 
 
-    xit('sorting of topics is persistent', function () {
+    it('sorting of topics is persistent', function () {
         E2ETopics.addTopicToMinutes('some topic');
         E2ETopics.addTopicToMinutes('some other topic');
         E2ETopics.addTopicToMinutes('yet another topic');
 
-        browser.dragAndDrop('#topicPanel .well:nth-child(3) .topicDragDropHandle', '#topicPanel .well:nth-child(1)');
+        browser.waitForExist('#topicPanel .well:nth-child(3) .topicDragDropHandle');
+        browser.moveToObject('#topicPanel .well:nth-child(3) .topicDragDropHandle');
+        browser.buttonDown();
+        browser.moveTo(1, 1);
+        browser.moveToObject('#topicPanel .well:nth-child(1)');
+        browser.buttonUp();
 
         var topicsBeforeReload = E2ETopics.getTopicsForMinute();
         let firstElementBeforeReload = topicsBeforeReload[0].ELEMENT;
