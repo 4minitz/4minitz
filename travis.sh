979c144fabd23143dee56a0adbee50ebc0ec4ead
Michael Gerhaeuser <michael.gerhaeuser@gmail.com>
2016-07-29 20:05:46 +0200

    Wait for meteor in a more flexible way
    This way we can possibly speed up builds using the cache but won't
    run into trouble on new branches or when meteor is updated.

--- travis.sh
+++ travis.sh
@@ -12,17 +12,33 @@ fi
 
 echo Run end2end tests: "$TEST"
 
-WAITTIME=480
-if [ -d "$DIRECTORY" ]; then
-    echo Meteor local exists, use shorter timeout?
-fi
+echo Remove old log file
+rm meteor.log
 
 echo Start end2end server
-npm run test:end2end:server&
+npm run test:end2end:server > meteor.log&
 
-echo Wait for server
-sleep $WAITTIME
+COUNTER=0
+MAX_WAIT=900
+until grep "=> App running at" meteor.log; do
+    echo App has not started yet.. Waiting for $COUNTER seconds
+    sleep 30
+    COUNTER=$(($COUNTER+30))
+
+    if [ $COUNTER -gt $MAX_WAIT ]; then
+        echo Meteor takes too long to start, exiting. Meteor log:
+        cat meteor.log
+        exit 1
+    fi
+done
 
 echo Start end2end test runner
 chimp --ddp=http://localhost:3100 --mocha --path=tests/end2end --browser=phantomjs -- $TEST tests/end2end/setup.js
 
+CHIMP_RESULT=$?
+
+echo Meteor server log:
+cat meteor.log
+rm meteor.log
+
+exit $CHIMP_RESULT
