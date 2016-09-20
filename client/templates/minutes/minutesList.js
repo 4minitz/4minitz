5daa78d98e0c7bd4bec70d5e0ce3d37ee008e553
Wolfram Esser <github@derwok.de>
2016-09-20 09:20:20 +0200

    WIP: Leave Meeting Series
    And removed some unused unreachable legacy "delete minutes" code.

--- client/templates/minutes/minutesList.js
+++ client/templates/minutes/minutesList.js
@@ -29,33 +29,19 @@ Template.minutesList.helpers({
 });
 
 Template.minutesList.events({
-    "click #deleteMinutes": function (evt) {
-        let disabled = evt.currentTarget.getAttribute('disabled');
-        if (disabled) return;
+    "click #btnLeaveMeetingSeries": function () {
+        console.log("User: "+Meteor.user()+" is leaving Meeting Series" + this.meetingSeries_id);
 
-        console.log("Remove Meeting Minute " + this._id + " from Series: " + this.meetingSeries_id);
-
-        let dialogContent = "<p>Do you really want to delete this meeting minute dated on <strong>" + this.date + "</strong>?</p>";
-        let newTopicsCount = this.getNewTopics().length;
-        if (newTopicsCount > 0) {
-            dialogContent += "<p>This will remove <strong>" + newTopicsCount
-                + " Topics</strong>, which were created within this minute.</p>";
-        }
-        let closedOldTopicsCount = this.getOldClosedTopics().length;
-        if (closedOldTopicsCount > 0) {
-            let additionally = (newTopicsCount > 0) ? "Additionally " : "";
-            dialogContent += "<p>" + additionally + "<strong>" + closedOldTopicsCount
-                + " topics</strong> will be opened again, which were closed whithin this minute.</p>"
-        }
-
+        let dialogContent = "<p>Do you really want to leave this meeting series?</p>";
         confirmationDialog(
             /* callback called if user wants to continue */
             () => {
-                let ms = new MeetingSeries(this.meetingSeries_id);
-                ms.removeMinutesWithId(this._id);
             },
-            /* Dialog content */
-            dialogContent
+            dialogContent,
+            "Leave Meeting Series",
+            "Leave",
+            "btn-warning"
         );
+
     }
 });
