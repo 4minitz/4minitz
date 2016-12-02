8faa6ec655d49ca69e9cdc04687ac40aa204d5b2
Felix Blechschmitt <fblechschmitt@hiorg-server.de>
2016-12-02 14:19:43 +0100

    TopicFilter use QueryParser
    The TopicFilter-Module needs a QueryParser which can be passed via dependency injection as constructor argument. This parser can now be used to parse the search query for special keywords or simple search tokens

--- imports/TopicFilter.js
+++ imports/TopicFilter.js
@@ -2,29 +2,49 @@
 
 export class TopicFilter {
 
-    constructor() {
-        this.currentQuery = null;
+    constructor(queryParser) {
+        if (null === queryParser || undefined === queryParser) {
+            throw new Meteor.Error('illegal-state', 'Please inject a query parser.');
+        }
         this.currentTopics = null;
+        this.parser = queryParser;
     }
 
     filter(topics, query) {
         this.currentTopics = topics;
-        this.currentQuery = query;
-        let result;
+        this.parser.parse(query);
 
-
-        result = this._filterSubject();
-        this.currentQuery = null;
+        this.currentTopics = this._filterForSearchTokens(this.parser.getSearchTokens());
+        let result = this.currentTopics;
         this.currentTopics = null;
+        this.parser.reset();
         return result;
     }
 
-    _filterSubject() {
-        if (null === this.currentQuery || null === this.currentTopics) {
+    /**
+     *
+     * @param searchTokens string[]|string
+     * @returns {*}
+     * @private
+     */
+    _filterForSearchTokens(searchTokens) {
+        if (null === this.currentTopics) {
             throw new Meteor.Error('illegal-state', 'Current state invalid. Query or topics-array null.');
         }
+        if (typeof searchTokens === 'string') {
+            searchTokens = [searchTokens];
+        }
 
-        let infoItemFilter = item => item.subject.indexOf(this.currentQuery) !== -1;
+        let infoItemFilter = item => {
+            for (let i=0; i < searchTokens.length; i++) {
+                let token = searchTokens[i];
+                if (item.subject.indexOf(token) === -1) {
+                    return false;
+                }
+            }
+            return true;
+            //return item.subject.indexOf(query) !== -1;
+        };
 
         return this.currentTopics
             .filter(topic => topic.infoItems.some(infoItemFilter))
--- tests/unit/imports/TopicFilter.test.js
+++ tests/unit/imports/TopicFilter.test.js
@@ -6,24 +6,36 @@ const {
     TopicFilter
     } = proxyquire('../../../imports/TopicFilter', {});
 
+let QueryParserMock = function() { this.query = null};
+QueryParserMock.prototype.parse = function(query) {
+    this.query = query;
+};
+QueryParserMock.prototype.getSearchTokens = function() {
+    return this.query.split(" ");
+};
+QueryParserMock.prototype.getFilterTokens = function() {
+    return [];
+};
+QueryParserMock.prototype.reset = function() {};
+
 describe('TopicFilter', function() {
 
     let topics, topicFilter;
 
     beforeEach(function() {
-        topicFilter = new TopicFilter();
+        topicFilter = new TopicFilter(new QueryParserMock());
         topics = [
             {
                 subject: "One",
-                infoItems: [{subject: "One.one"}, {subject: "One.two"}]
+                infoItems: [{subject: "one.one"}, {subject: "one.two"}]
             },
             {
                 subject: "Two",
-                infoItems: [{subject: "Two.one"}, {subject: "Two.two"}, {subject: "Two.three"}]
+                infoItems: [{subject: "two.one"}, {subject: "two.two"}, {subject: "two.three"}]
             },
             {
                 subject: "Three",
-                infoItems: [{subject: "Three.one"}, {subject: "Three.two"}, {subject: "Three.three"}, {subject: "Three.four"}]
+                infoItems: [{subject: "three.one"}, {subject: "three.two"}, {subject: "three.three"}, {subject: "three.four"}]
             }
         ];
     });
@@ -40,7 +52,15 @@ describe('TopicFilter', function() {
     it('returns the filtered array of topics', function() {
         let res = topicFilter.filter(topics, "three");
 
-        expect(res, "Length of the result topic array should be 3").have.length(2);
+        expect(res, "Length of the result topic array should be 2").have.length(2);
+        expect(res[0].infoItems, "The first topic should contain one info items").to.have.length(1);
+        expect(res[1].infoItems, "The 2nd topic should contain four info items").to.have.length(4);
+    });
+
+    it('can filter for multiple search tokens', function() {
+        let res = topicFilter.filter(topics, "three two");
+
+        expect(res, "Length of the result topic array should be 2").have.length(2);
         expect(res[0].infoItems, "The first topic should contain one info items").to.have.length(1);
         expect(res[1].infoItems, "The 2nd topic should contain one info items").to.have.length(1);
     });
