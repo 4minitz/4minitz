262bad91f22e50dad693398edae7b14c41cf7a4a
Wolfram Esser <github@derwok.de>
2017-02-05 18:37:32 +0100

    Fix: Unit: no working meteor in unit tests

--- server/ldap.js
+++ server/ldap.js
@@ -39,10 +39,12 @@ LDAP.bindValue = function (usernameOrEmail, isEmailAddress) {
 
     // #Security
     // If users have been imported with importUsers.js and "isInactivePredicate" was used to
-    // make some users isInactive==true - we stop them from loggin in here.
-    let checkUserInactive = Meteor.users.findOne({username: usernameOrEmail});
-    if (checkUserInactive && checkUserInactive.isInactive) {
-        throw new Meteor.Error(403, "User is inactive");
+    // make some users isInactive==true - we stop them from logging in here.
+    if (Meteor && Meteor.users) {   // skip test during unit tests
+        let checkUserInactive = Meteor.users.findOne({username: usernameOrEmail});
+        if (checkUserInactive && checkUserInactive.isInactive) {
+            throw new Meteor.Error(403, "User is inactive");
+        }
     }
 
     return [searchDn, '=', username, ',', serverDn].join('');
