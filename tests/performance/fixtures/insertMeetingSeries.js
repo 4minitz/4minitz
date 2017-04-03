c33e484b111b6cd1f0ec31c9674d70f7fe35eafc
Felix Blechschmitt <fblechschmitt@hiorg-server.de>
2017-04-03 17:36:07 +0200

    Customized minutes seeding tool by passing parameters

--- tests/performance/fixtures/insertMeetingSeries.js
+++ tests/performance/fixtures/insertMeetingSeries.js
@@ -3,15 +3,29 @@ import { MinutesGenerator } from './generators/minutes-generator';
 import { TopicsGenerator } from './generators/topics-generator';
 import { MongoDb } from './lib/mongo-db';
 import { Collections } from './lib/mongo-db';
+import { RangeHelper } from './lib/range-helper';
 
+/* Define parameters */
+let optionParser = require('node-getopt').create([
+    ['m', 'mongourl=[ARG]', 'Mongo DB url (default: mongodb://localhost:3101/meteor)'],
+    ['u', 'username=[ARG]', 'The username of the user who should be the owner of the created minutes (default: user1)'],
+    ['', 'minutes-count=[ARG]', 'The number of minutes which should be created'],
+    ['', 'topics-range=[ARG]', 'The range of topics each minutes should contain (e.g. 3-10)'],
+    ['', 'items-range=[ARG]', 'The range of items each topic should contain (e.g. 1-8)'],
+    ['', 'details-range=[ARG]', 'The range of sentences each detail should contain (e.g. 7-23)'],
+    ['h', 'help', 'Display this help']
+]);
 
-const USERNAME = 'user1';
-const MONGO_URL = 'mongodb://localhost:3101/meteor';
+/* Initialize parameter parser */
+let arg = optionParser.bindHelp().parseSystem();
+
+const USERNAME = arg.options.username || 'user1';
+const MONGO_URL = arg.options.mongourl || 'mongodb://localhost:3101/meteor';
 const CONFIG = {
-    minutesCount: 5,
-    topicsRange: {min: 3, max: 10},
-    itemsRange: {min: 1, max: 8},
-    detailsSentenceRange: {min: 7, max: 23} // number of sentences per detail
+    minutesCount: parseInt(arg.options['minutes-count'], 10) || 5,
+    topicsRange: RangeHelper.convertRangeToMinMaxObject(arg.options['topics-range'], {min: 3, max: 10}),
+    itemsRange: RangeHelper.convertRangeToMinMaxObject(arg.options['items-range'], {min: 1, max: 8}),
+    detailsSentenceRange: RangeHelper.convertRangeToMinMaxObject(arg.options['details-range'], {min: 7, max: 23}) // number of sentences per detail
 };
 
 
---
+++ tests/performance/fixtures/lib/range-helper.js
@@ -0,0 +1,25 @@
+
+export class RangeHelper {
+
+    static convertRangeToMinMaxObject(range, fallbackResult) {
+        let min, max;
+
+        if (isNaN(range)) {
+            let array = (range) ? range.split('-') : [];
+            if (array.length > 1) {
+                min = parseInt(array[0], 10);
+                max = parseInt(array[1], 10);
+            } else {
+                return fallbackResult;
+            }
+        } else {
+            min = max = parseInt(range, 10);
+        }
+
+        return {
+            min: min,
+            max: max
+        };
+    }
+
+}
