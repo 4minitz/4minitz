2354fef5bd92c665382523437219f4bb5865f31f
Felix Blechschmitt <fblechschmitt@hiorg-server.de>
2017-04-06 21:26:27 +0200

    ResponsiblePreparer refactoring step four: Split big functions into smaller ones

--- imports/ResponsiblePreparer.js
+++ imports/ResponsiblePreparer.js
@@ -1,9 +1,13 @@
 export class ResponsiblePreparer {
 
+    /**
+     * @typedef {{id: string, text: string}} ResponsibleObject
+     */
+
     constructor(minutes, currentTopicOrItem, usersCollection, freeTextValidator = undefined) {
         this.minutes = minutes;
         this.parentSeries = minutes.parentMeetingSeries();
-        this.currentTopicOrItem = currentTopicOrItem;
+        this.currentElement = currentTopicOrItem;
         this.usersCollection = usersCollection;
         this.freeTextValidator = freeTextValidator;
 
@@ -14,10 +18,16 @@ export class ResponsiblePreparer {
         this.remainingUsers = [];
     }
 
+    /**
+     * @return {ResponsibleObject[]}
+     */
     getPossibleResponsibles() {
         return this.possibleResponsibles;
     }
 
+    /**
+     * @return {ResponsibleObject[]}
+     */
     getRemainingUsers() {
         return this.remainingUsers;
     }
@@ -28,60 +38,26 @@ export class ResponsiblePreparer {
     }
 
     _preparePossibleResponsibles() {
-        // add regular participants from current minutes
-        for (let i in this.minutes.participants) {
-            this._addUserIdToBuffer(this.minutes.participants[i].userId);
-        }
+        this._addRegularParticipantsFromCurrentMinutes();
+        this._addAdditionalParticipantsFromMinutesAsFreetext();
+        this._addFormerResponsiblesFromParentSeries();
+        this._addResponsiblesFromCurrentElement();
+        this._pruneDuplicatesAndPrepareResult();
+    }
 
-        // add the "additional participants" from current minutes as simple strings
+    _addRegularParticipantsFromCurrentMinutes() {
+        this.minutes.participants.forEach(participant => {
+            this.buffer.push(participant.userId);
+        });
+    }
+
+    _addAdditionalParticipantsFromMinutesAsFreetext() {
         let participantsAdditional = this.minutes.participantsAdditional;
         if (participantsAdditional) {
-            let splitted = participantsAdditional.split(/[,;]/);
-            for (let i in splitted) {
-                this._addFreeTextElementToBuffer(splitted[i].trim());
-            }
+            participantsAdditional.split(/[,;]/).forEach(freeText => {
+                this._addFreeTextElementToBuffer(freeText.trim());
+            });
         }
-
-        // add former responsibles from the parent meeting series
-        this._addFormerResponsiblesFromParentSeries();
-
-
-        // add the responsibles from current topic
-        if (this.currentTopicOrItem && this.currentTopicOrItem.hasResponsibles()) {
-            this.buffer = this.buffer.concat(this.currentTopicOrItem._topicDoc.responsibles);
-        }
-
-        // copy buffer to possibleResponsibles
-        // but take care for uniqueness
-        for (let i in this.buffer) {
-            let aResponsibleId = this.buffer[i];
-            if (! this.possibleResponsiblesUnique[aResponsibleId]) { // not seen?
-                this.possibleResponsiblesUnique[aResponsibleId] = true;
-                let aResponsibleName = aResponsibleId;
-                let aUser = this.usersCollection.findOne(aResponsibleId);
-                if (aUser) {
-                    aResponsibleName = aUser.username;
-                    if (aUser.profile && aUser.profile.name && aUser.profile.name !== "") {
-                        aResponsibleName += " - "+aUser.profile.name;
-                    }
-                }
-                this.possibleResponsibles.push({id: aResponsibleId, text: aResponsibleName});
-            }
-        }
-    }
-
-    _addUserIdToBuffer(userid) {
-        this.buffer.push(userid);
-    }
-
-    _addFreeTextElementToBuffer(text) {
-        if (this._isValidFreeTextElement(text)) {
-            this.buffer.push(text);
-        }
-    }
-
-    _isValidFreeTextElement(text) {
-        return (!this.freeTextValidator || this.freeTextValidator(text));
     }
 
     _addFormerResponsiblesFromParentSeries() {
@@ -97,25 +73,78 @@ export class ResponsiblePreparer {
         }
     }
 
-    _prepareRemainingUsers() {
-        let participantsIds = [];
-        for (let i in this.possibleResponsibles) {
-            if (this.possibleResponsibles[i].id && this.possibleResponsibles[i].id.length > 15) {   // Meteor _ids default to 17 chars
-                participantsIds.push(this.possibleResponsibles[i].id);
+    _addResponsiblesFromCurrentElement() {
+        if (this.currentElement && this.currentElement.hasResponsibles()) {
+            this.buffer = this.buffer.concat(this.currentElement._topicDoc.responsibles);
+        }
+    }
+
+    _pruneDuplicatesAndPrepareResult() {
+        this.buffer.forEach(userIdOrFreeText => {
+            if (!this.possibleResponsiblesUnique[userIdOrFreeText]) {
+                this.possibleResponsiblesUnique[userIdOrFreeText] = true;
+                this.possibleResponsibles.push(this._createResponsibleObject(userIdOrFreeText));
+            }
+        });
+    }
+
+    /**
+     *
+     * @param userIdOrFreeTextOrUserObject
+     * @return {ResponsibleObject}
+     * @private
+     */
+    _createResponsibleObject(userIdOrFreeTextOrUserObject) {
+        let user = userIdOrFreeTextOrUserObject;
+        if (typeof userIdOrFreeTextOrUserObject === 'string') {
+            user = this.usersCollection.findOne(userIdOrFreeTextOrUserObject);
+            if (!user) {
+                return {id: userIdOrFreeTextOrUserObject, text: userIdOrFreeTextOrUserObject};
             }
         }
 
-        // format return object suiting for select2.js
-        let users = this.usersCollection.find(
+        return {id: user._id, text: ResponsiblePreparer._formatUser(user)};
+    }
+
+    static _formatUser(user) {
+        let usertext = user.username;
+        if (user.profile && user.profile.name && user.profile.name !== "") {
+            usertext += ` - ${user.profile.name}`;
+        }
+        return usertext;
+    }
+
+    _addFreeTextElementToBuffer(text) {
+        if (!this.freeTextValidator || this.freeTextValidator(text)) {
+            this.buffer.push(text);
+        }
+    }
+
+    _prepareRemainingUsers() {
+        let users = this._fetchUsersWhichAreNoPossibleResponsible();
+        this._addUsersToRemainingList(users);
+    }
+
+    _fetchUsersWhichAreNoPossibleResponsible() {
+        let participantsIds = [];
+        this.possibleResponsibles.forEach(possibleResponsible => {
+            if (ResponsiblePreparer._responsibleMightBeID(possibleResponsible)) {
+                participantsIds.push(possibleResponsible.id);
+            }
+        });
+
+        return this.usersCollection.find(
             {$and: [{_id: {$nin: participantsIds}},
                 {isInactive: {$not: true}}]}).fetch();
+    }
 
-        for (let i in users) {
-            let usertext = users[i].username;
-            if (users[i].profile && users[i].profile.name && users[i].profile.name !== "") {
-                usertext += " - "+users[i].profile.name;
-            }
-            this.remainingUsers.push ({id: users[i]._id, text: usertext});
-        }
+    static _responsibleMightBeID(value) {
+        return (value.id && value.id.length > 15);   // Meteor _ids default to 17 chars
+    }
+
+    _addUsersToRemainingList(users) {
+        users.forEach(user => {
+            this.remainingUsers.push(this._createResponsibleObject(user));
+        });
     }
 }
