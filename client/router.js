064add8bf9efe177b3b9f3ad05ca99596631958c
Felix Blechschmitt <fblechschmitt@hiorg-server.de>
2017-04-07 20:09:24 +0200

    Removed route `/minutesadd/:_id`
    Creating a minutes will now be done directly in the template event `click #btnAddMinutes` of the tabMinutesList-Template

--- client/router.js
+++ client/router.js
@@ -1,7 +1,5 @@
 import { FlowRouter } from 'meteor/kadira:flow-router';
 import { BlazeLayout } from 'meteor/kadira:blaze-layout';
-import { MeetingSeries } from '/imports/meetingseries';
-import { UserRoles } from '/imports/userroles';
 
 FlowRouter.route('/', {
     action() {
@@ -35,52 +33,6 @@ FlowRouter.route('/meetingseries/:_id', {
         BlazeLayout.render('appLayout', {main: 'meetingSeriesDetails'});
     }
 });
-
-// todo: get rid of this
-// this should not be handled within a route
-FlowRouter.route('/minutesadd/:_id', {
-    name: 'minutesadd',
-    action(params) {
-        let meetingSeriesID = params._id;
-
-        let usrRoles = new UserRoles();
-        if (!usrRoles.hasViewRoleFor(meetingSeriesID)) {
-            FlowRouter.go("/");
-        }
-
-        let id;
-        ms = new MeetingSeries(meetingSeriesID);
-        ms.addNewMinutes(
-            // optimistic ui callback
-            newMinutesID => {
-                id = newMinutesID
-            },
-
-            // server callback
-            (error/*, newMinutesID*/) => {
-                // no need to redirect to correct minutes page
-                // as the optimistic ui callback already took
-                // care of that
-
-                if (error) {
-                    // display error
-                    Session.set("errorTitle", error.error);
-                    Session.set("errorReason", error.reason);
-                    FlowRouter.redirect('/meetingseries/' + meetingSeriesID);
-                }
-            }
-        );
-
-        // callback should have been called by now
-        if (id) {
-            FlowRouter.redirect('/minutesedit/' + id);
-        } else {
-            // todo: use error page
-            FlowRouter.redirect('/meetingseries/' + meetingSeriesID);
-        }
-    }
-});
-
 
 FlowRouter.route('/minutesedit/:_id', {
     name: 'minutesedit',
