4minitz/4minitz

commit 7a69b85cbfa721b7cd8a9c6db26adf2c10e21d2d

MeetingSeries-View wait until subscription is ready - Fixes #214

client/templates/meetingseries/meetingSeriesDetails.js
@@ -13,14 +13,15 @@ import { TabTopicsConfig } from './tabTopics'
 let _meetingSeriesID;   // the parent meeting object of this minutes
 
 Template.meetingSeriesDetails.onCreated(function () {
+    this.seriesReady = new ReactiveVar();
+
     this.autorun(() => {
         _meetingSeriesID = FlowRouter.getParam('_id');
         this.showSettingsDialog = FlowRouter.getQueryParam('edit') === 'true';
 
-        let usrRoles = new UserRoles();
-        if (!usrRoles.hasViewRoleFor(_meetingSeriesID)) {
-            FlowRouter.go('/');
-        }
+        let subscriptionHandle = this.subscribe('meetingSeries', _meetingSeriesID);
+
+        this.seriesReady.set(subscriptionHandle.ready());
     });
 
     this.activeTabTemplate = new ReactiveVar("tabMinutesList");
@@ -35,6 +36,16 @@ Template.meetingSeriesDetails.onRendered(function () {
 });
 
 Template.meetingSeriesDetails.helpers({
+    authenticating() {
+        const subscriptionReady = Template.instance().seriesReady.get();
+        return Meteor.loggingIn() || !subscriptionReady;
+    },
+    redirectIfNotAllowed() {
+        let usrRoles = new UserRoles();
+        if (!usrRoles.hasViewRoleFor(_meetingSeriesID)) {
+            FlowRouter.go('/');
+        }
+    },
     meetingSeries: function() {
         return new MeetingSeries(_meetingSeriesID);
     },

client/templates/meetingseries/meetingSeriesDetails.html
@@ -1,51 +1,55 @@
 <template name="meetingSeriesDetails">
+    {{#if authenticating}}
+        {{> loading}}
+    {{else}}
+        {{redirectIfNotAllowed}}
+        {{#with meetingSeries}}
+            <!--Include modal dialog for edit meeting series -->
+            {{> meetingSeriesEdit this}}
 
-    {{#with meetingSeries}}
-        <!--Include modal dialog for edit meeting series -->
-        {{> meetingSeriesEdit this}}
+            <h2 class="header">
+                <span class="hidden-xs">Meeting Series:</span>
+                {{project}} - {{name}}
+                {{#if isModerator}}
+                    <a href="#" data-toggle="modal" data-target="#dlgEditMeetingSeries"
+                       id="btnEditMeetingSeries"
+                       class="pull-right"
+                       title="Configure Meeting Series">
 
-        <h2 class="header">
-            <span class="hidden-xs">Meeting Series:</span>
-            {{project}} - {{name}}
-            {{#if isModerator}}
-                <a href="#" data-toggle="modal" data-target="#dlgEditMeetingSeries"
-                   id="btnEditMeetingSeries"
-                   class="pull-right"
-                   title="Configure Meeting Series">
+                        <span class="glyphicon glyphicon-cog" aria-hidden="true" aria-label="Edit Meeting Series"></span>
+                    </a>
+                {{/if}}
+            </h2>
+        {{/with}}
 
-                    <span class="glyphicon glyphicon-cog" aria-hidden="true" aria-label="Edit Meeting Series"></span>
-                </a>
-            {{/if}}
-        </h2>
-    {{/with}}
+        {{> error}}
 
-    {{> error}}
-
-    <ul class="nav nav-tabs">
-        <li id="tab_minutes" data-template="tabMinutesList" role="presentation" class="{{isTabActive 'tab_minutes'}}"><a href="#">Minutes</a></li>
-        <li id="tab_topics" data-template="tabTopics" data-action="topics" role="presentation" class="{{isTabActive 'tab_topics'}}"><a href="#">Topics</a></li>
-        <li id="tab_items" data-template="tabItems" data-action="items" role="presentation" class="{{isTabActive 'tab_items'}}"><a href="#">Items</a></li>
-    </ul>
-    <div class="active">
-        {{> Template.dynamic template=tab data=tabData}}
-    </div>
-
-    <p></p>
-    <a class="btn btn-primary btn-raised" href="{{pathFor '/'}}">
-        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true" aria-label="Go Back"></span>
-        Go Back
-    </a>
-
-    <br>
-    <br>
-
-    {{#if showQuickHelp}}
-        <div class="help panel panel-default">
-            <div class="panel-heading">Quick Help</div>
-            <div class="panel-body">
-                <p>Here you may create new minutes for the current meeting series or click on a date link of existing minutes.</p>
-                <a href="#" id="btnHideHelp">OK, hide</a>
-            </div>
+        <ul class="nav nav-tabs">
+            <li id="tab_minutes" data-template="tabMinutesList" role="presentation" class="{{isTabActive 'tab_minutes'}}"><a href="#">Minutes</a></li>
+            <li id="tab_topics" data-template="tabTopics" data-action="topics" role="presentation" class="{{isTabActive 'tab_topics'}}"><a href="#">Topics</a></li>
+            <li id="tab_items" data-template="tabItems" data-action="items" role="presentation" class="{{isTabActive 'tab_items'}}"><a href="#">Items</a></li>
+        </ul>
+        <div class="active">
+            {{> Template.dynamic template=tab data=tabData}}
         </div>
+
+        <p></p>
+        <a class="btn btn-primary btn-raised" href="{{pathFor '/'}}">
+            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true" aria-label="Go Back"></span>
+            Go Back
+        </a>
+
+        <br>
+        <br>
+
+        {{#if showQuickHelp}}
+            <div class="help panel panel-default">
+                <div class="panel-heading">Quick Help</div>
+                <div class="panel-body">
+                    <p>Here you may create new minutes for the current meeting series or click on a date link of existing minutes.</p>
+                    <a href="#" id="btnHideHelp">OK, hide</a>
+                </div>
+            </div>
+        {{/if}}
     {{/if}}
 </template>

commit abea7c27d5dba856bf3384d1edd30f5d143e63f8

Fix: Meeting Series Editor now opens again after creation of new meeting series
The dialog auto-open was broken after commit 7a69b85cbfa721b7cd8a9c6db26adf2c10e21d2d of  fixing issue #214.

client/templates/meetingseries/meetingSeriesDetails.js
@@ -31,7 +31,11 @@ Template.meetingSeriesDetails.onCreated(function () {
 Template.meetingSeriesDetails.onRendered(function () {
     if (this.showSettingsDialog) {
         Session.set("meetingSeriesEdit.showUsersPanel", true);
-        $('#dlgEditMeetingSeries').modal('show');
+
+        // Defer opening the meeting series settings dialog after rendering of the template
+        window.setTimeout(function () {
+            $('#dlgEditMeetingSeries').modal('show');
+        }, 10);
     }
 });
 