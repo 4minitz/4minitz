c13447c82fdf241233e4cd31e8a971277e1e5cf0
Michael Gerhaeuser <michael.gerhaeuser@gmail.com>
2017-04-18 20:02:35 +0200

    Explicitly import markdown

--- client/main.client.js
+++ client/main.client.js
@@ -1,4 +1,5 @@
 import { Meteor } from 'meteor/meteor';
+import { Markdown } from 'meteor/perak:markdown';
 import { bootstrapAttachementsLiveQuery } from '/imports/collections/attachments_private'
 
 // initialize
