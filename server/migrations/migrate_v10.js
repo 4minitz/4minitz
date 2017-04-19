3957f3de5316ab841bf8744e8e46fbe3101536d4
Michael Gerhaeuser <michael.gerhaeuser@gmail.com>
2017-04-19 11:15:23 +0200

    Break up the circular dependency in migrate_v10
    MigrateV10 had dependencies to MigrateSeriesUp and vice versa. To break
    this circular dependency two static methods were extracted from the
    MigrateV10 class into the local context of the module. There they can
    be accessed by both classes without both depending on each other.

--- server/migrations/migrate_v10.js
+++ server/migrations/migrate_v10.js
@@ -1,63 +1,30 @@
 import { MinutesCollection } from '/imports/collections/minutes_private';
 import { MeetingSeriesCollection } from '/imports/collections/meetingseries_private';
 
-// add "createdInMinute" attribute for topics
-// --> update all existing topics in all minutes and meeting series!
-export class MigrateV10 {
+function saveSeries(series) {
+    MeetingSeriesCollection.update(
+        series._id,
+        {
+            $set: {
+                "topics": series.topics,
+                "openTopics": series.openTopics
+            }
+        },
+        { bypassCollection2: true }
+    );
+}
 
-    static up() {
-        console.log('% Progress - updating all topics. This might take several minutes...');
-        let allSeries = MeetingSeriesCollection.find();
-        allSeries.forEach(series => {
-            (new MigrateSeriesUp(series)).run();
-        });
-    }
-
-    static down() {
-        MeetingSeriesCollection.find().forEach(series => {
-            series.topics = MigrateV10._downgradeTopics(series.topics);
-            series.openTopics = MigrateV10._downgradeTopics(series.openTopics);
-            MigrateV10.saveSeries(series);
-        });
-        MinutesCollection.find().forEach(minutes => {
-            minutes.topics = MigrateV10._downgradeTopics(minutes.topics);
-            MigrateV10.saveMinutes(minutes);
-        });
-    }
-
-    static _downgradeTopics(topics) {
-        topics.forEach(topic => {
-            delete topic.createdInMinute;
-        });
-        return topics;
-    }
-
-
-    static saveSeries(series) {
-        MeetingSeriesCollection.update(
-            series._id,
-            {
-                $set: {
-                    "topics": series.topics,
-                    "openTopics": series.openTopics
-                }
-            },
-            {bypassCollection2: true}
-        );
-    }
-
-    static saveMinutes(minutes) {
-        // We switch off bypassCollection2 here, to skip .clean & .validate to allow empty string values
-        MinutesCollection.update(
-            minutes._id,
-            {
-                $set: {
-                    "topics": minutes.topics,
-                }
-            },
-            {bypassCollection2: true}
-        );
-    }
+function saveMinutes(minutes) {
+    // We switch off bypassCollection2 here, to skip .clean & .validate to allow empty string values
+    MinutesCollection.update(
+        minutes._id,
+        {
+            $set: {
+                "topics": minutes.topics,
+            }
+        },
+        { bypassCollection2: true }
+    );
 }
 
 class MigrateSeriesUp {
@@ -70,11 +37,11 @@ class MigrateSeriesUp {
         let minutes = this.series.firstMinutes();
         while (minutes) {
             minutes = this._updateTopicsOfMinutes(minutes);
-            MigrateV10.saveMinutes(minutes);
+            saveMinutes(minutes);
             minutes = minutes.nextMinutes();
         }
         this._updateTopicsOfSeries();
-        MigrateV10.saveSeries(this.series);
+        saveSeries(this.series);
     }
 
     /**
@@ -111,5 +78,37 @@ class MigrateSeriesUp {
         this.series.openTopics.forEach(topic => {
             this._updateTopic(topic, false /*all topics should already exist in map!*/);
         });
+    }
+}
+
+// add "createdInMinute" attribute for topics
+// --> update all existing topics in all minutes and meeting series!
+export class MigrateV10 {
+
+    static up() {
+        console.log('% Progress - updating all topics. This might take several minutes...');
+        let allSeries = MeetingSeriesCollection.find();
+        allSeries.forEach(series => {
+            (new MigrateSeriesUp(series)).run();
+        });
+    }
+
+    static down() {
+        MeetingSeriesCollection.find().forEach(series => {
+            series.topics = MigrateV10._downgradeTopics(series.topics);
+            series.openTopics = MigrateV10._downgradeTopics(series.openTopics);
+            saveSeries(series);
+        });
+        MinutesCollection.find().forEach(minutes => {
+            minutes.topics = MigrateV10._downgradeTopics(minutes.topics);
+            saveMinutes(minutes);
+        });
+    }
+
+    static _downgradeTopics(topics) {
+        topics.forEach(topic => {
+            delete topic.createdInMinute;
+        });
+        return topics;
     }
 }
