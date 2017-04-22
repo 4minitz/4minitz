491b636b83a0299b4cd9d501ba60c15fac15fbee
ampasc <patrick.schwarzfischer@gmx.de>
2017-04-22 22:10:33 +0200

    Feature/#210 simple user profile dialog (#222)
    * Enh: Edit User Profile (Feature/#210 simple user profile dialog)
    
    Added a dialog to edit a user profile
    
    * Enh:#210-Simple-User-Profile-Dialog 
    
    Added a user profile dialog, which allows editing user's long name and email. Added e2e tests for password change and profile edit
    
    * Fix:  added e2e User-test to Travis
    
    Added e2e User-test to travis.yml
    
    * Doc: improved userguide
    
    Added descriptions for the following features: Warning if no participants selected, Edit user profile. Moved a User 2e2-test to the 1st Travis-container
    
    * Fix: changed editProfile Server Method
    
    added following functionality: admin-user can edit all profiles, ordinary user can't edit profile data of other users. Fixed Edit Profile e2e-test
    
    * Fix: Fixed E2E-Test which failed due to a conflict with a previous test case
    
    * Revert "Merge branch 'feature/#210-Simple-User-Profile-Dialog' into develop"
    
    This reverts commit aca967446c65c69350d5dd1677b9f9e0f0399f79.
    
    * Re-add feature210 Simple User Profile Dialog
    
    * Fix: Adapted feature 210 to the new base which was injected by PullRequest-4
    
    * Fix: implemented several Issues which were requested on the pull request
    
    Edit-Profile-Methods supports now Optimistic-UI and is forbidden for LDAP-Users.
    Fixed several problems related to the e2e tests

---
+++ client/templates/globals/profileEditDialog.js
@@ -0,0 +1,66 @@
+import { Meteor } from 'meteor/meteor';
+import { FlashMessage } from '../../helpers/flashMessage';
+import { addCustomValidator } from '../../helpers/customFieldValidator'; 
+import { emailAddressRegExpTest } from '/imports/helpers/email';
+
+let showError = function (evt, error) {
+    (new FlashMessage('Error', error.reason)).show();
+    evt.preventDefault();
+};
+
+let checkEMailIsValid = (email) => {
+    return emailAddressRegExpTest.test(eMail);
+};
+
+Template.profileEditDialog.onRendered(function() { 
+    addCustomValidator( 
+        "#id_emailAddress", 
+        (value) => { return checkPasswordMatchesPattern(value) }, 
+        'Not a valid E-Mail address'); 
+}); 
+
+Template.profileEditDialog.events({
+    "submit #frmDlgEditProfile"(evt, tmpl) {
+        evt.preventDefault();
+
+        if (!Meteor.user()) {
+            return;
+        }
+
+        if (Meteor.user().isDemoUser) { 
+            return; 
+        } 
+        
+        let uLongName = tmpl.find("#id_longName").value;
+        let uEmailAddress = tmpl.find("#id_emailAddress").value;
+
+        tmpl.$("#btnEditProfileSave").prop("disabled",true);
+
+        Meteor.call('users.editProfile', Meteor.userId(), uEmailAddress,uLongName, function (error, result) {
+            if (error) {
+                tmpl.$("#btnEditProfileSave").prop("disabled",false);
+                console.log(error);
+                showError(evt, error);
+            } else {
+                (new FlashMessage('OK', "Profile edited.", 'alert-success', 2000)).show();
+                Meteor.setTimeout(function () {
+                    $('#dlgEditProfile').modal("hide");
+                }, 2000);
+            }
+        });
+
+    },
+
+    "show.bs.modal #dlgEditProfile": function (evt, tmpl) {
+        let usr = Meteor.users.findOne(Meteor.userId());
+        if (usr.profile){
+            tmpl.find("#id_longName").value = usr.profile.name;
+        }
+        tmpl.find("#id_emailAddress").value = usr.emails[0].address;
+        tmpl.$("#btnEditProfileSave").prop("disabled",false);
+    },
+
+    "shown.bs.modal #dlgEditProfile": function (evt, tmpl) {
+        tmpl.find("#id_longName").focus();
+    }
+});
