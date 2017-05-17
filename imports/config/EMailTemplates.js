1463209c1852e418edef0f1e20ed49d2fee019e9
Robert Zimmermann <robnexx92@gmail.com>
2017-05-17 12:02:14 +0200

    Get Default Email from config file

--- imports/config/EMailTemplates.js
+++ imports/config/EMailTemplates.js
@@ -1,17 +1,20 @@
+import { GlobalSettings } from '../config/GlobalSettings';
 import { Accounts } from 'meteor/accounts-base'
 
-Accounts.emailTemplates.siteName = "4Minitz";
-Accounts.emailTemplates.from     = "4Minitz <mail@4minitz.com>";
+Accounts.emailTemplates.siteName = '4Minitz';
+Accounts.emailTemplates.from     = Accounts.emailTemplates.siteName + "<" + GlobalSettings.getDefaultEmailSenderAddress() + ">";
 
 Accounts.emailTemplates.verifyEmail = {
     subject() {
-        return "[4Minitz] Verify Your Email Address";
+        return "[" + Accounts.emailTemplates.siteName + "] Verify Your Email Address";
     },
     text( user, url ) {
         let emailAddress   = user.emails[0].address,
             urlWithoutHash = url.replace( '#/', '' ),
-            supportEmail   = "mail@4minitz.com",
-            emailBody      = `To verify your email address (${emailAddress}) visit the following link:\n\n${urlWithoutHash}\n\n If you did not request this verification, please ignore this email. If you feel something is wrong, please contact our support team: ${supportEmail}.`;
+            emailBody      = 'To verify your email address ' + emailAddress + ' visit the following link:\n\n' +
+                            urlWithoutHash +
+                            '\n\n If you did not request this verification, please ignore this email. ' +
+                            'If you feel something is wrong, please contact your admin team';
 
         return emailBody;
     }
@@ -19,13 +22,15 @@ Accounts.emailTemplates.verifyEmail = {
 
 Accounts.emailTemplates.resetPassword = {
     subject() {
-        return "[4Minitz] Reset Your Password";
+        return "[" + Accounts.emailTemplates.siteName + "] Reset Your Password";
     },
     text( user, url ) {
         let emailAddress   = user.emails[0].address,
             urlWithoutHash = url.replace( '#/', '' ),
-            supportEmail   = "mail@4minitz.com",
-            emailBody      = `To reset your password for (${emailAddress}) visit the following link:\n\n${urlWithoutHash}\n\n If you did not request to reset your password, please ignore this email. If you feel something is wrong, please contact our support team: ${supportEmail}.`;
+            emailBody      = 'To reset your password for ' + emailAddress + ' visit the following link:\n\n' +
+                            urlWithoutHash +
+                            '\n\n If you did not request to reset your password, please ignore this email. ' +
+                            'If you feel something is wrong, please contact your admin team';
 
         return emailBody;
     }
