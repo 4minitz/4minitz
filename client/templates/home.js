54da7a6ba6ee7776d6c9cd6272b7a4761971f191
Robert Zimmermann <robnexx92@gmail.com>
2017-05-17 08:35:11 +0200

    Changed login behavior
    User with not verficated email only can trigger resend verification link

--- client/templates/home.js
+++ client/templates/home.js
@@ -1,3 +1,16 @@
 Template.home.created = function () {
     //add your statement here 
 };
+
+Template.home.events({
+    'click .resend-verification-link' () {
+        Meteor.call('sendVerificationLink', (error) => {
+            if (error) {
+                alert(error.reason);
+            } else {
+                let email = Meteor.user().emails[0].address;
+                alert("Verification sent to " + email + "");
+            }
+        });
+    }
+});
