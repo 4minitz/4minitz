a99c568f08250b6e786c0122d6307f42cd872429
Wolfram Esser <github@derwok.de>
2017-05-25 18:06:26 +0200

    Clarify error for LDAP users on try to reset password

--- server/sendResetPasswordMail.js
+++ server/sendResetPasswordMail.js
@@ -1,8 +1,13 @@
+import { Meteor } from 'meteor/meteor';
+
+// We wrap the original Accounts.sendResetPasswordEmail to perform
+// a check for user.isLDAPuser in advance. Because LDAP users
+// should not be able to change their passwords via this WebApp.
 let originalSendResetPasswordEmail = Accounts.sendResetPasswordEmail;
 Accounts.sendResetPasswordEmail = function (userId, email) {
     let user = Meteor.users.findOne(userId);
     if (user.isLDAPuser) {
-        throw new Meteor.Error( 418, 'You are not allowed to reset your password' );
+        throw new Meteor.Error( 418, 'LDAP users are not allowed to reset their password with \'Standard\' login' );
     }
     originalSendResetPasswordEmail(userId, email);
 };
