5fab9c4b95c6535b08196c1de849023b1ded75b9
Robert Zimmermann <robnexx92@gmail.com>
2017-05-27 18:25:35 +0200

    Updated demo user settings
    Demo user needs to be verified when it exists
    Added db migration to update demo user verifications state

---
+++ server/migrations/migrate_v14.js
@@ -0,0 +1,22 @@
+import { Meteor} from 'meteor/meteor';
+
+export class MigrateV14 {
+
+    static up() {
+        let demoUser = Meteor.users.findOne({'username': 'demo'});
+        if (demoUser) {
+            Meteor.users.update({'username': 'demo'}, {$set: {'emails[0].vrified': true}});
+            if (demoUser.emails[0].verified === undefined) {
+                Meteor.users.update({'username': 'demo'}, {$set: {'emails[0].verified': true}});
+            }
+        }
+    }
+
+    static down() {
+        let demoUser = Meteor.users.findOne({'username': 'demo'});
+        if (demoUser) {
+            Meteor.users.update({'username': 'demo'}, {$set: {'emails[0].verified': false}});
+        }
+    }
+
+}
