e67821bfb0967978b581cfaa3f3ed0f2959929ce
amPasc <patrick.schwarzfischer@gmx.de>
2017-05-29 10:00:29 +0200

    fix: Removed Tabs

--- tests/end2end/MeetingSeries_security.js
+++ tests/end2end/MeetingSeries_security.js
@@ -15,24 +15,24 @@ describe('MeetingSeries Security', function () {
     });
 
     it('can not insert a new MeetingSerie if not logged in @watch', function (done) {
-		const methodName = 'meetingseries.insert';
-		
-		expect(E2EApp.isLoggedIn()).to.be.false;
+        const methodName = 'meetingseries.insert';
+        
+        expect(E2EApp.isLoggedIn()).to.be.false;
 
         const aProjectName = "Hacker Project";
         const aMeetingName = "Hacker Meeting #1";
 
-		E2ESecurity.expectMethodToExist(methodName);
-		E2ESecurity.replaceMethodOnClientSide(methodName);
-		
-		let noOfMeetingSeries = server.call('e2e.countMeetingSeriesInMongDB');
-		
-		E2ESecurity.callMethodWithCallback(true, "meetingseries.insert", {project: aProjectName, name: aMeetingName});
-		expect(server.call('e2e.countMeetingSeriesInMongDB')).to.equal(noOfMeetingSeries);
-		
-		
-		E2EApp.loginUser();
-		E2ESecurity.callMethodWithCallback(false, "meetingseries.insert", {project: aProjectName, name: aMeetingName});
-		expect(server.call('e2e.countMeetingSeriesInMongDB')).to.equal(noOfMeetingSeries + 1);
+        E2ESecurity.expectMethodToExist(methodName);
+        E2ESecurity.replaceMethodOnClientSide(methodName);
+        
+        let noOfMeetingSeries = server.call('e2e.countMeetingSeriesInMongDB');
+        
+        E2ESecurity.callMethodWithCallback(true, "meetingseries.insert", {project: aProjectName, name: aMeetingName});
+        expect(server.call('e2e.countMeetingSeriesInMongDB')).to.equal(noOfMeetingSeries);
+        
+        
+        E2EApp.loginUser();
+        E2ESecurity.callMethodWithCallback(false, "meetingseries.insert", {project: aProjectName, name: aMeetingName});
+        expect(server.call('e2e.countMeetingSeriesInMongDB')).to.equal(noOfMeetingSeries + 1);
     });
 });
--- tests/end2end/helpers/E2ESecurity.js
+++ tests/end2end/helpers/E2ESecurity.js
@@ -6,39 +6,39 @@ const errorSessionVariableName = 'LastMethodCallError';
 export class E2ESecurity {
     static expectMethodToExist(methodName){
         let methodExists = browser.execute( function(methodName) {
-			return typeof Meteor.connection._methodHandlers[methodName] === 'function'
-			
+            return typeof Meteor.connection._methodHandlers[methodName] === 'function'
+            
         },methodName).value;
-		expect(methodExists, 'Method ' + methodName + ' exists').to.be.true;
+        expect(methodExists, 'Method ' + methodName + ' exists').to.be.true;
     }
-	
-	static replaceMethodOnClientSide(methodName) {
-		browser.execute( function(methodName) {
-			Meteor.connection._methodHandlers[methodName] = function () {console.log('Modified Client Method: ' + methodName);};
-		}, methodName);
-	}
-	
-	static callMethodWithCallback(expectToFail, methodName, ...methodParameters) {
-		//------------------------------------------------------
-		// NEW CODE: Fancy but not working
-		//------------------------------------------------------
-		browser.execute(function(expectToFail, methodName, methodParameters) {
-				Meteor.apply(methodName, methodParameters, {}, function(error, result) {
-					if (expectToFail && !error) {
-						expect.fail('By calling the method ' + methodName + ' an error was expected but did not occur'); 
-					}
-					if (!expectToFail && error) {
-						expect.fail('Error within method call for ' + methodName + ':  ' + error.reason); 
-					}
-					console.log('DEKA');
-					done();
-				});
+    
+    static replaceMethodOnClientSide(methodName) {
+        browser.execute( function(methodName) {
+            Meteor.connection._methodHandlers[methodName] = function () {console.log('Modified Client Method: ' + methodName);};
+        }, methodName);
+    }
+    
+    static callMethodWithCallback(expectToFail, methodName, ...methodParameters) {
+        //------------------------------------------------------
+        // NEW CODE: Fancy but not working
+        //------------------------------------------------------
+        browser.execute(function(expectToFail, methodName, methodParameters) {
+                Meteor.apply(methodName, methodParameters, {}, function(error, result) {
+                    if (expectToFail && !error) {
+                        expect.fail('By calling the method ' + methodName + ' an error was expected but did not occur'); 
+                    }
+                    if (!expectToFail && error) {
+                        expect.fail('Error within method call for ' + methodName + ':  ' + error.reason); 
+                    }
+                    console.log('DEKA');
+                    done();
+                });
             }, expectToFail, methodName, methodParameters);
-		//------------------------------------------------------
-		//OLD CODE: Working but stupid
-		//------------------------------------------------------
-		/*
-		// This function uses session variables to transmit data between the client, the server and the test runner
+        //------------------------------------------------------
+        //OLD CODE: Working but stupid
+        //------------------------------------------------------
+        /*
+        // This function uses session variables to transmit data between the client, the server and the test runner
         // it will return an object with two properties, the error and the result of the called method        
         browser.execute( function(methodName, methodParameters, resultSessionVariableName, errorSessionVariableName) {
             //Clear Session Variables to determine the methods end by the changes in the session variables
@@ -73,7 +73,6 @@ export class E2ESecurity {
         expect(!!result || !!error, 'Method ' + methodName + ' was executed in time').to.be.true;
         //return error and result, one of them has to be filled
         return { result : result, error: error}
+        */
     }
-		*/
-	}
 }
