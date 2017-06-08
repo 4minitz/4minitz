c56857f656af8155f1e22555e4e9f96921dfcfd0
Felix Blechschmitt <fblechschmitt@hiorg-server.de>
2017-06-08 16:55:35 +0200

    MeetingSeriesList: Added helper to receive lastMinutes form MinutesFinder-Server - Fixes #272
    Before we used the 'lastMinutes' method of the minutes class which was moved to the MinutesFinder-Service.

--- client/templates/meetingseries/meetingSeriesList.js
+++ client/templates/meetingseries/meetingSeriesList.js
@@ -1,5 +1,6 @@
 import { MeetingSeries } from '/imports/meetingseries';
 import { UserRoles } from '/imports/userroles';
+import { MinutesFinder } from '../../../imports/services/minutesFinder';
 
 Template.meetingSeriesList.helpers({
     meetingSeriesRow: function () {
@@ -11,5 +12,10 @@ Template.meetingSeriesOverview.helpers({
     isModeratorOfSeries: function () {
         let usrRole = new UserRoles();
         return usrRole.isModeratorOf(Template.instance().data._id);
+    },
+
+    lastMinutes() {
+        const seriesDocumentFromDataContext = this;
+        return MinutesFinder.lastMinutesOfMeetingSeries(seriesDocumentFromDataContext);
     }
 });
--- tests/end2end/MeetingSeries-test.js
+++ tests/end2end/MeetingSeries-test.js
@@ -1,6 +1,7 @@
-import { E2EGlobal } from './helpers/E2EGlobal'
-import { E2EApp } from './helpers/E2EApp'
-import { E2EMeetingSeries } from './helpers/E2EMeetingSeries'
+import { E2EGlobal } from './helpers/E2EGlobal';
+import { E2EApp } from './helpers/E2EApp';
+import { E2EMeetingSeries } from './helpers/E2EMeetingSeries';
+import { E2EMinutes } from './helpers/E2EMinutes';
 
 
 describe('MeetingSeries', function () {
@@ -15,9 +16,9 @@ describe('MeetingSeries', function () {
     });
 
     it('can create a first meeting series', function () {
-        let aProjectName = "E2E Project";
-        let aMeetingName = "Meeting Name #1";
-        let initialCount = E2EMeetingSeries.countMeetingSeries();
+        const aProjectName = "E2E Project";
+        const aMeetingName = "Meeting Name #1";
+        const initialCount = E2EMeetingSeries.countMeetingSeries();
         E2EMeetingSeries.createMeetingSeries(aProjectName, aMeetingName);
         expect(E2EMeetingSeries.countMeetingSeries()).to.equal(initialCount + 1);
         expect(E2EMeetingSeries.getMeetingSeriesId(aProjectName, aMeetingName)).to.be.ok;
@@ -25,9 +26,9 @@ describe('MeetingSeries', function () {
 
 
     it('can create a further meeting series', function () {
-        let aProjectName = "E2E Project";
-        let aMeetingName = "Meeting Name #2";
-        let initialCount = E2EMeetingSeries.countMeetingSeries();
+        const aProjectName = "E2E Project";
+        const aMeetingName = "Meeting Name #2";
+        const initialCount = E2EMeetingSeries.countMeetingSeries();
         E2EMeetingSeries.createMeetingSeries(aProjectName, aMeetingName);
         expect(E2EMeetingSeries.countMeetingSeries()).to.equal(initialCount + 1);
         expect(E2EMeetingSeries.getMeetingSeriesId(aProjectName, aMeetingName)).to.be.ok;
@@ -35,9 +36,9 @@ describe('MeetingSeries', function () {
 
 
     it.skip('can submit the form by pressing enter in the meetingname input', function () {
-        let aProjectName = "E2E Project";
-        let aMeetingName = "Meeting Name #2.7182818284";
-        let initialCount = E2EMeetingSeries.countMeetingSeries();
+        const aProjectName = "E2E Project";
+        const aMeetingName = "Meeting Name #2.7182818284";
+        const initialCount = E2EMeetingSeries.countMeetingSeries();
 
         E2EMeetingSeries.editMeetingSeriesForm(aProjectName, aMeetingName + "\n");
 
@@ -49,31 +50,43 @@ describe('MeetingSeries', function () {
 
 
     it('can not create meeting series with empty project', function () {
-        let aProjectName = "";
-        let aMeetingName = "Meeting Name #2.1";
-        let initialCount = E2EMeetingSeries.countMeetingSeries();
+        const aProjectName = "";
+        const aMeetingName = "Meeting Name #2.1";
+        const initialCount = E2EMeetingSeries.countMeetingSeries();
         E2EMeetingSeries.createMeetingSeries(aProjectName, aMeetingName);
         expect(E2EMeetingSeries.countMeetingSeries()).to.equal(initialCount);
         expect(E2EMeetingSeries.getMeetingSeriesId(aProjectName, aMeetingName)).not.to.be.ok;
     });
 
     it('can not create meeting series with empty name', function () {
-        let aProjectName = "E2E Project";
-        let aMeetingName = "";
-        let initialCount = E2EMeetingSeries.countMeetingSeries();
+        const aProjectName = "E2E Project";
+        const aMeetingName = "";
+        const initialCount = E2EMeetingSeries.countMeetingSeries();
         E2EMeetingSeries.createMeetingSeries(aProjectName, aMeetingName);
         expect(E2EMeetingSeries.countMeetingSeries()).to.equal(initialCount);
         expect(E2EMeetingSeries.getMeetingSeriesId(aProjectName, aMeetingName)).not.to.be.ok;
     });
 
     it('can goto meeting series details', function () {
-        let aProjectName = "E2E Project";
-        let aMeetingName = "Meeting Name #4";
+        const aProjectName = "E2E Project";
+        const aMeetingName = "Meeting Name #4";
         E2EMeetingSeries.createMeetingSeries(aProjectName, aMeetingName);
         E2EMeetingSeries.gotoMeetingSeries(aProjectName, aMeetingName);
         expect(E2EApp.isOnStartPage()).to.be.false;
     });
 
+    it('displays the date of the last minutes in the meeting series overview', function() {
+        const aProjectName = "E2E Minutes";
+        const aMeetingName = "Meeting Name #4";
+        const myDate = "2015-03-17";  // date of first project commit ;-)
+
+        E2EMeetingSeries.createMeetingSeries(aProjectName, aMeetingName);
+        E2EMinutes.addMinutesToMeetingSeries(aProjectName, aMeetingName, myDate);
+
+        E2EMeetingSeries.doSomethingWithMeetingSeriesListItem(aProjectName, aMeetingName, '', (elemId) => {
+            expect(browser.elementIdText(elemId).value).to.have.string(`(last minutes of ${myDate}`);
+        });
+    });
 
     it.skip('can submit the form by pressing enter in the project name input', function () {
         let aProjectName = "E2E Project";
