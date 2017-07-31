8e0255a5ca80e01cbb8a25a3b5780c5f8aeb7cd3
iCatCat <olga.icat@gmail.com>
2017-07-31 12:18:57 +0200

    added tests for workflow.removeMinute

--- tests/end2end/Minutes_security.js
+++ tests/end2end/Minutes_security.js
@@ -10,6 +10,7 @@ describe('MeetingSeries Security', function () {
     const insertMeetingSeriesMethod = "meetingseries.insert";
     const updateMinutes = "minutes.update";
     const addMinutes = "workflow.addMinutes";
+    const removeMinute = "workflow.removeMinute";
 
     beforeEach("goto start page and make sure test user is logged in", function () {
         E2EApp.gotoStartPage();
@@ -149,7 +150,7 @@ describe('MeetingSeries Security', function () {
         expect((server.call('e2e.countMinutesInMongoDB'))).to.equal(numberOfMinutes);
     });
 
-    it('can not add a new Minute if not invited to a Meeting Serie @watch', function () {
+    it('can not add a new Minute if not invited to a Meeting Serie', function () {
         const aProjectName = "MinuteAdd as not invited to MS";
         const aMeetingName = "MinuteAdd as not invited to MS";
         E2ESecurity.executeMethod(insertMeetingSeriesMethod, {project: aProjectName, name: aMeetingName});
@@ -165,5 +166,70 @@ describe('MeetingSeries Security', function () {
         E2ESecurity.executeMethod(addMinutes, {meetingSeries_id: meetingSeriesID, date: date, visibleFor:[userid]});
         expect((server.call('e2e.countMinutesInMongoDB'))).to.equal(numberOfMinutes);
     });
+
+    //workflow.removeMinute
+    it('can delete a Minute if a moderator', function () {
+        const aProjectName = "MinuteDelete as moderator";
+        const aMeetingName = "MinuteDelete as moderator";
+        E2ESecurity.executeMethod(insertMeetingSeriesMethod, {project: aProjectName, name: aMeetingName});
+        E2EMinutes.addMinutesToMeetingSeries(aProjectName, aMeetingName);
+        const minuteID =  E2EMinutes.getCurrentMinutesId();
+        const numberOfMinutes = server.call('e2e.countMinutesInMongoDB');
+
+        E2ESecurity.replaceMethodOnClientSide(removeMinute);
+        E2ESecurity.executeMethod(removeMinute, minuteID);
+        expect((server.call('e2e.countMinutesInMongoDB'))).to.equal(numberOfMinutes-1);
+    });
+
+    it('can not delete a Minute if not logged in', function () {
+        const aProjectName = "MinuteDelete as not logged in";
+        const aMeetingName = "MinuteDelete as not logged in";
+        E2ESecurity.executeMethod(insertMeetingSeriesMethod, {project: aProjectName, name: aMeetingName});
+        E2EMinutes.addMinutesToMeetingSeries(aProjectName, aMeetingName);
+        const minuteID =  E2EMinutes.getCurrentMinutesId();
+        const numberOfMinutes = server.call('e2e.countMinutesInMongoDB');
+
+        E2EApp.logoutUser();
+        E2ESecurity.replaceMethodOnClientSide(removeMinute);
+        E2ESecurity.executeMethod(removeMinute, minuteID);
+        expect((server.call('e2e.countMinutesInMongoDB'))).to.equal(numberOfMinutes);
+    });
+
+    it('can not delete a Minute if not invited to a Meeting Serie', function () {
+        const aProjectName = "MinuteDelete as not invited to MS";
+        const aMeetingName = "MinuteDelete as not invited to MS";
+        E2ESecurity.executeMethod(insertMeetingSeriesMethod, {project: aProjectName, name: aMeetingName});
+        E2EMinutes.addMinutesToMeetingSeries(aProjectName, aMeetingName);
+        const minuteID =  E2EMinutes.getCurrentMinutesId();
+        const numberOfMinutes = server.call('e2e.countMinutesInMongoDB');
+
+        E2EApp.logoutUser();
+        E2EApp.loginUser(1);
+        E2ESecurity.replaceMethodOnClientSide(removeMinute);
+        E2ESecurity.executeMethod(removeMinute, minuteID);
+        expect((server.call('e2e.countMinutesInMongoDB'))).to.equal(numberOfMinutes);
+    });
+
+    it('can not delete a Minute as an invited user @watch', function () {
+        const aProjectName = "MinuteDelete as an invited user";
+        const aMeetingName = "MinuteDelete as as an invited user";
+        E2ESecurity.executeMethod(insertMeetingSeriesMethod, {project: aProjectName, name: aMeetingName});
+
+        E2EMeetingSeriesEditor.openMeetingSeriesEditor(aProjectName, aMeetingName, "invited");
+        let user2 = E2EGlobal.SETTINGS.e2eTestUsers[1];
+        E2EMeetingSeriesEditor.addUserToMeetingSeries(user2, E2EGlobal.USERROLES.Invited);
+        E2EMeetingSeriesEditor.closeMeetingSeriesEditor();
+
+        E2EMinutes.addMinutesToMeetingSeries(aProjectName, aMeetingName);
+        const minuteID =  E2EMinutes.getCurrentMinutesId();
+        const numberOfMinutes = server.call('e2e.countMinutesInMongoDB');
+
+        E2EApp.logoutUser();
+        E2EApp.loginUser(1);
+        E2ESecurity.replaceMethodOnClientSide(removeMinute);
+        E2ESecurity.executeMethod(removeMinute, minuteID);
+        expect((server.call('e2e.countMinutesInMongoDB'))).to.equal(numberOfMinutes);
+    });
+
 
     });
