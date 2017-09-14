f983a5704931b36035296d1075367548e04a9cd0
Michael Gerhaeuser <michael.gerhaeuser@methodpark.de>
2017-09-14 11:27:33 +0200

    Replace session variable with reactivevar
    By removing the session variable and binding it to the template instance we automatically reset the filter as soon as the template is reloaded/replaced.

--- client/templates/meetingseries/meetingSeriesSearch.js
+++ client/templates/meetingseries/meetingSeriesSearch.js
@@ -1,21 +1,11 @@
 import { Template } from 'meteor/templating';
 import { MeetingSeries } from '/imports/meetingseries';
-import { Session } from 'meteor/session';
 
 Template.meetingSeriesSearch.events({
     'keyup .meetingSeriesSearchbar': function (event) {
         const target = event.currentTarget;
         const text = target.searchMeetingSeries.value;
-        Session.set('search-query', text);
+
+        Template.instance().data.updateSearchQuery(text);
     }
 });
-
-Template.meetingSeriesSearch.searchResults = function () {
-    let wholeString = Session.get('search-query');
-    let split = wholeString.match(/[^\s]+/g);
-    if (!split) {
-        split = [];
-    }
-    let query = new RegExp(split.join('|'), 'i');
-    return MeetingSeries.find({$or:[{'name': query}, {'project': query}]});
-};
