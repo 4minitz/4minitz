f2902dd23025bf8304ef092a196c017c9f91db15
Felix Blechschmitt <fblechschmitt@hiorg-server.de>
2017-09-14 17:01:20 +0200

    Adapt minutes seeding tool
    Added new fields createdAt/createdBy/updatedAt/updatedBy for topics, items and details and createdBy for minutes.

--- client/helpers/dateHelpers.js
+++ client/helpers/dateHelpers.js
@@ -4,3 +4,7 @@ import {formatDateISO8601Time} from '../../imports/helpers/date';
 Template.registerHelper('formatDateISO8601Time', (date) => {
     return formatDateISO8601Time(date);
 });
+
+Template.registerHelper('formateUpdatedHint', (date, username) => {
+    return `Updated ${formatDateISO8601Time(date)}` + ((username) ? ` by ${username}` : '');
+});
