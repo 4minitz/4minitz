a16b91c5f8fa9f4de7a453614679006bda10e2e1
Wolfram Esser <github@derwok.de>
2017-09-14 22:41:57 +0200

    E2E: Fix createMeetingSeries()
    meetingSeriesID must be calculated before route change. Otherwise attachment E2E is broken

--- tests/end2end/helpers/E2EMeetingSeries.js
+++ tests/end2end/helpers/E2EMeetingSeries.js
@@ -41,15 +41,16 @@ export class E2EMeetingSeries {
         E2EGlobal.clickWithRetry('#btnAddInvite');
         E2EGlobal.waitSomeTime(1000);  // additional time for deferred dialog open + panel switch!
 
+        let meetingSeriesID = browser.getUrl();
+        meetingSeriesID = meetingSeriesID.replace(/^.*\//, "");
+        meetingSeriesID = meetingSeriesID.replace(/\?.*$/, "");
+
         if (! keepOpenMSEditor) {
             E2EGlobal.clickWithRetry('#btnMeetinSeriesEditCancel');
             E2EGlobal.waitSomeTime();
             E2EApp.gotoStartPage();
         }
 
-        let meetingSeriesID = browser.getUrl();
-        meetingSeriesID = meetingSeriesID.replace(/^.*\//, "");
-        meetingSeriesID = meetingSeriesID.replace(/\?.*$/, "");
         return meetingSeriesID;
     };
 
