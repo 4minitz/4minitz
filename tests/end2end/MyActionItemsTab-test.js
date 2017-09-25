50744fb864bacc36706f551640e637e64ae548fd
Jessica Wöstmann <jessica.woestmann@freenet.de>
2017-09-25 13:25:11 +0200

    Fiexed E2E test

--- tests/end2end/MyActionItemsTab-test.js
+++ tests/end2end/MyActionItemsTab-test.js
@@ -8,7 +8,6 @@ describe('MyActionItems Tab', function () {
     const aProjectName = "MyActionItems Tab";
     let aMeetingCounter = 0;
     let aMeetingNameBase = "Meeting Name #";
-    let aMeetingName;
 
     before("reload page and reset app", function () {
         E2EApp.resetMyApp(true);
@@ -18,43 +17,41 @@ describe('MyActionItems Tab', function () {
     beforeEach("goto start page and make sure test user is logged in", function () {
         E2EApp.gotoStartPage();
         expect(E2EApp.isLoggedIn()).to.be.true;
-
-        aMeetingCounter++;
-        aMeetingName = aMeetingNameBase + aMeetingCounter;
     });
 
     it("can filter my action items from all meeting series @watch", function () {
 
         this.timeout(400000);
-        var meetingName = aMeetingName + '1';
+
+        var meetingName = aMeetingNameBase + '1';
         E2EMeetingSeries.createMeetingSeries(aProjectName, meetingName);
 
         E2EMinutes.addMinutesToMeetingSeries(aProjectName, meetingName);
         E2ETopics.addTopicToMinutes('topic #1');
-        E2ETopics.addInfoItemToTopic({subject: 'action item  #1', itemType: "actionItem"}, 1);
+        E2ETopics.addInfoItemToTopic({subject: 'action item  #1', itemType: "actionItem", responsible: "user1"}, 1);
         E2EMinutes.finalizeCurrentMinutes();
 
         E2EMinutes.addMinutesToMeetingSeries(aProjectName, meetingName);
         E2ETopics.addTopicToMinutes('topic #2');
-        E2ETopics.addInfoItemToTopic({subject: 'action item  #2', itemType: "actionItem"}, 1);
+        E2ETopics.addInfoItemToTopic({subject: 'action item  #2', itemType: "actionItem", responsible: "user1"}, 1);
         E2EMinutes.finalizeCurrentMinutes();
 
-        meetingName = aMeetingName + '2';
+        meetingName = aMeetingNameBase + '2';
         E2EMeetingSeries.createMeetingSeries(aProjectName, meetingName);
 
         E2EMinutes.addMinutesToMeetingSeries(aProjectName, meetingName);
         E2ETopics.addTopicToMinutes('topic #3');
-        E2ETopics.addInfoItemToTopic({subject: 'action item  #3', itemType: "actionItem"}, 1);
+        E2ETopics.addInfoItemToTopic({subject: 'action item  #3', itemType: "actionItem", responsible: "user1"}, 1);
         E2EMinutes.finalizeCurrentMinutes();
 
         E2EMinutes.addMinutesToMeetingSeries(aProjectName, meetingName);
         E2ETopics.addTopicToMinutes('topic #4');
-        E2ETopics.addInfoItemToTopic({subject: 'action item  #4', itemType: "actionItem"}, 1);
+        E2ETopics.addInfoItemToTopic({subject: 'action item  #4', itemType: "actionItem", responsible: "user1"}, 1);
         E2EMinutes.finalizeCurrentMinutes();
 
         E2EApp.gotoStartPage();
 
-        E2EMeetingSeries.gotoTabItems();
+        E2EApp.gotoActionItemsTab();
 
         expect(E2ETopics.countItemsForTopic('#itemPanel'), "Items list should have four items").to.equal(4);
 
