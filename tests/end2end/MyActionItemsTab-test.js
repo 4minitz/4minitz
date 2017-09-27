9b74009c18073e3c7c81511cbce6dbdd3d6fc447
Jessica Wöstmann <jessica.woestmann@freenet.de>
2017-09-27 11:02:52 +0200

    Tried to fix E2E test and added another one

--- tests/end2end/MyActionItemsTab-test.js
+++ tests/end2end/MyActionItemsTab-test.js
@@ -19,7 +19,10 @@ describe('MyActionItems Tab', function () {
         expect(E2EApp.isLoggedIn()).to.be.true;
     });
 
-    it("can filter my action items from all meeting series @watch", function () {
+    it("can filter my action items from all meeting series", function () {
+
+        this.timeout(120000);
+
         let meetingName = aMeetingNameBase + '1';
         E2EMeetingSeries.createMeetingSeries(aProjectName, meetingName);
 
@@ -50,5 +53,22 @@ describe('MyActionItems Tab', function () {
         E2EApp.gotoActionItemsTab();
 
         expect(E2ETopics.countItemsForTopic('#itemPanel'), "Items list should have four items").to.equal(4);
+    });
+
+    it("can filter my action items from all action items", function () {
+        let meetingName = aMeetingNameBase + '1';
+        E2EMeetingSeries.createMeetingSeries(aProjectName, meetingName);
+
+        E2EMinutes.addMinutesToMeetingSeries(aProjectName, meetingName);
+        E2ETopics.addTopicToMinutes('topic #1');
+        E2ETopics.addInfoItemToTopic({subject: 'action item  #2', itemType: "actionItem", responsible: E2EApp.getCurrentUser()}, 1);
+        E2ETopics.addInfoItemToTopic({subject: 'action item  #1', itemType: "actionItem", responsible: (E2EApp.getCurrentUser() + ',' + E2EGlobal.SETTINGS.e2eTestUsers[1])}, 1);
+        E2ETopics.addInfoItemToTopic({subject: 'action item  #2', itemType: "actionItem", responsible: E2EGlobal.SETTINGS.e2eTestUsers[2]}, 1);
+        E2EMinutes.finalizeCurrentMinutes();
+
+        E2EApp.gotoStartPage();
+        E2EApp.gotoActionItemsTab();
+
+        expect(E2ETopics.countItemsForTopic('#itemPanel'), "Items list should have two items").to.equal(2);
     });
 });
