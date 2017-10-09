b69892b6301131bfde551daff83e6f343d7f76f2
Jessica Wöstmann <jessica.woestmann@freenet.de>
2017-10-09 11:11:54 +0200

    Fixed E2E test

--- tests/end2end/MyActionItemsTab-test.js
+++ tests/end2end/MyActionItemsTab-test.js
@@ -28,12 +28,12 @@ describe('MyActionItems Tab', function () {
 
         E2EMinutes.addMinutesToMeetingSeries(aProjectName, meetingName);
         E2ETopics.addTopicToMinutes('topic #1');
-        E2ETopics.addInfoItemToTopic({subject: 'action item  #1', itemType: "actionItem", responsible: E2EApp.getCurrentUser()}, 1);
+        E2ETopics.addInfoItemToTopic({subject: 'action item #1', itemType: "actionItem", responsible: E2EApp.getCurrentUser()}, 1);
         E2EMinutes.finalizeCurrentMinutes();
 
         E2EMinutes.addMinutesToMeetingSeries(aProjectName, meetingName);
         E2ETopics.addTopicToMinutes('topic #2');
-        E2ETopics.addInfoItemToTopic({subject: 'action item  #2', itemType: "actionItem", responsible: E2EApp.getCurrentUser()}, 1);
+        E2ETopics.addInfoItemToTopic({subject: 'action item #2', itemType: "actionItem", responsible: E2EApp.getCurrentUser()}, 1);
         E2ETopics.toggleActionItem(1, 1);
         E2EMinutes.finalizeCurrentMinutes();
 
@@ -42,34 +42,33 @@ describe('MyActionItems Tab', function () {
 
         E2EMinutes.addMinutesToMeetingSeries(aProjectName, meetingName);
         E2ETopics.addTopicToMinutes('topic #3');
-        E2ETopics.addInfoItemToTopic({subject: 'action item  #3', itemType: "actionItem", responsible: E2EApp.getCurrentUser()}, 1);
+        E2ETopics.addInfoItemToTopic({subject: 'action item #3', itemType: "actionItem", responsible: E2EApp.getCurrentUser()}, 1);
         E2EMinutes.finalizeCurrentMinutes();
 
         E2EMinutes.addMinutesToMeetingSeries(aProjectName, meetingName);
         E2ETopics.addTopicToMinutes('topic #4');
-        E2ETopics.addInfoItemToTopic({subject: 'action item  #4', itemType: "actionItem", responsible: E2EApp.getCurrentUser()}, 1);
+        E2ETopics.addInfoItemToTopic({subject: 'action item #4', itemType: "actionItem", responsible: E2EApp.getCurrentUser()}, 1);
         E2EMinutes.finalizeCurrentMinutes();
 
         E2EApp.gotoStartPage();
         E2EApp.gotoActionItemsTab();
 
-        expect(E2ETopics.countItemsForTopic('#itemPanel'), "Items list should have four items").to.equal(3);
+        expect(E2ETopics.countItemsForTopic('#itemPanel'), "Items list should have three items").to.equal(3);
     });
 
     it("can filter my action items from all action items", function () {
-        let meetingName = aMeetingNameBase + '1';
+        let meetingName = aMeetingNameBase + '3';
         E2EMeetingSeries.createMeetingSeries(aProjectName, meetingName);
 
         E2EMinutes.addMinutesToMeetingSeries(aProjectName, meetingName);
-        E2ETopics.addTopicToMinutes('topic #1');
-        E2ETopics.addInfoItemToTopic({subject: 'action item  #2', itemType: "actionItem", responsible: E2EApp.getCurrentUser()}, 1);
-        E2ETopics.addInfoItemToTopic({subject: 'action item  #1', itemType: "actionItem", responsible: (E2EApp.getCurrentUser() + ',' + E2EGlobal.SETTINGS.e2eTestUsers[1])}, 1);
-        E2ETopics.addInfoItemToTopic({subject: 'action item  #2', itemType: "actionItem", responsible: E2EGlobal.SETTINGS.e2eTestUsers[2]}, 1);
+        E2ETopics.addTopicToMinutes('topic #5');
+        E2ETopics.addInfoItemToTopic({subject: 'action item #5', itemType: "actionItem", responsible: (E2EApp.getCurrentUser() + ',' + E2EGlobal.SETTINGS.e2eTestUsers[1])}, 1);
+        E2ETopics.addInfoItemToTopic({subject: 'action item #6', itemType: "actionItem", responsible: E2EGlobal.SETTINGS.e2eTestUsers[1]}, 1);
         E2EMinutes.finalizeCurrentMinutes();
 
         E2EApp.gotoStartPage();
         E2EApp.gotoActionItemsTab();
 
-        expect(E2ETopics.countItemsForTopic('#itemPanel'), "Items list should have two items").to.equal(2);
+        expect(E2ETopics.countItemsForTopic('#itemPanel'), "Items list should have four items").to.equal(4);
     });
 });
