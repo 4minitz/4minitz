d60d06c47aba7736b5c6e43c732f012392d945e3
symonra <dark_fishbone@gmx.de>
2017-11-04 18:48:22 +0100

    Fixed migration and added unit test

--- server/migrations/migrate_v20.js
+++ server/migrations/migrate_v20.js
@@ -1,47 +1,76 @@
 import { TopicSchema } from '/imports/collections/topic.schema';
-import { DetailsSchema } from '/imports/collections/infoitem.schema';
 import { MinutesSchema } from '/imports/collections/minutes.schema';
+
+function saveMinutes(minutes) {
+    // We switch off bypassCollection2 here, to skip .clean & .validate to allow empty string values
+    MinutesSchema.getCollection().update(
+        minutes._id, {
+            $set: {
+                'topics': minutes.topics,
+            }
+        }
+    );
+}
+
+function saveTopics(topics) {
+    TopicSchema.getCollection().update(
+        topics._id, {
+            $set: {
+                'infoItems': topics.infoItems,
+            }
+        }
+    );
+}
+
+function forEachDetailInTopics(topics, operation){
+    topics.forEach(topic =>{
+        topic.infoItems.forEach(infoItem => {
+            infoItem.details.forEach(operation);
+        });
+    });
+}
+
+function forEachDetailInMinutes(minutes, operation) {
+    minutes.forEach(min => {
+        min.topics.forEach(topic => {
+            topic.infoItems.forEach(infoItem => {
+                infoItem.details.forEach(operation);
+            });
+        });
+    });
+}
 
 // Details: add field: isNew
 export class MigrateV20 {
 
     static up() {
         let allTopics = TopicSchema.getCollection().find();
-        allTopics.forEach(topic => {
-            topic.infoItems.forEach(infoItem => {
-                infoItem.details.forEach(detail => {
-                    DetailsSchema.getCollection().update(
-                        detail._id,
-                        {
-                            $set: {isNew: false}
-                        },
-                        {bypassCollection2: true});
-                });
-            });
+        forEachDetailInTopics(allTopics, detail => {
+            detail.isNew = false;
         });
+        saveTopics(allTopics);
 
         let allMinutes = MinutesSchema.getCollection().find();
         allMinutes.forEach(min => {
-            min.topics.forEach(topic => {
-                topic.infoItems.forEach(infoItem => {
-                    infoItem.details.forEach(detail => {
-
-                        let isNew = (detail.createdInMinute === min._id);
-                        DetailsSchema.getCollection().update(
-                            detail._id,
-                            {
-                                $set: {isNew: isNew}
-                            },
-                            {bypassCollection2: true});
-                    });
-                });
+            forEachDetailInTopics(min.topics, detail => {
+                let isNew = (detail.createdInMinute === min._id);
+                detail.isNew = isNew;
             });
         });
+        saveMinutes(allMinutes);
     }
 
     static down() {
-        DetailsSchema.getCollection().update({},
-            {$unset: { isNew: false}},
-            {multi: true, bypassCollection2: true});
+        let allTopics = TopicSchema.getCollection().find();
+        forEachDetailInTopics(allTopics, detail => {
+            delete detail.isNew;
+        });
+        saveTopics(allTopics);
+
+        let allMinutes = MinutesSchema.getCollection().find();
+        forEachDetailInMinutes(allMinutes, detail => {
+            delete detail.isNew;
+        });
+        saveMinutes(allMinutes);
     }
 }
