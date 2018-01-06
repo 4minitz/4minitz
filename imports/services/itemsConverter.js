6266aa94ddd43d42b14296e938ffda49ae754eae
Wolfram Esser <github@derwok.de>
2018-01-06 14:53:13 +0100

    Fix: isOpen and isSticky attributes needed caretaking on conversion Info => Action
    If optional isOpen attribute is not yet available, we must set it to true, otherwise the AI will be considered "done" immediately after conversion.
     So, a "done" state is preserved when converting back-and-forth.
    
    As the isSticky attribute is not optional, and action items are not allowed to have isSticky attribute, we set it to false here, without condition.
    So, the isSticky state is lost when conferting back-and-forth

--- imports/services/itemsConverter.js
+++ imports/services/itemsConverter.js
@@ -8,6 +8,17 @@ export class ItemsConverter {
             infoOrActionItem._infoItemDoc.itemType = 'infoItem';
         } else if (infoOrActionItem instanceof InfoItem) {
             infoOrActionItem._infoItemDoc.itemType = 'actionItem';
+
+            // If optional isOpen attribute is not yet available, we must set it to true,
+            // otherwise the AI will be considered "done" immediately after conversion.
+            // So, a "done" state is preserved when converting back-and-forth.
+            if (infoOrActionItem._infoItemDoc.isOpen === undefined) {
+                infoOrActionItem._infoItemDoc.isOpen = true;
+            }
+            // As the isSticky attribute is not optional, and action items
+            // are not allowed to have isSticky attribute, we set it to false here, without condition.
+            // So, the isSticky state is lost when conferting back-and-forth
+            infoOrActionItem._infoItemDoc.isSticky = false;
         }
         await infoOrActionItem.save();
     }
