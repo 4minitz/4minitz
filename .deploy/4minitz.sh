a49ccf60cc7c1a1d81e5ec73d77c40491805fb7d
Michael Gerhaeuser <michael.gerhaeuser@gmail.com>
2020-01-25 16:09:58 +0100

    Improve Docker build scripts
    [skip ci]

--- .deploy/4minitz.sh
+++ .deploy/4minitz.sh
@@ -3,6 +3,14 @@
 basedir4min=/4minitz_storage
 settingsfile=${basedir4min}/4minitz_settings.json
 
+if [[ ! -d $basedir4min ]]
+then
+    echo "Could not find $basedir4min. This script is intended as an entrypoint"
+    echo "for the 4Minitz Docker container. To start the app without Docker"
+    echo "adjust the `./deploy/run_sample.sh` script and use that."
+    exit 1
+fi
+
 # Settings file, initial copy from container to host once
 if [ -f "$settingsfile" ]
 then
