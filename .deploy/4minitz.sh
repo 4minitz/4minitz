d7e713905b7cd505be239b2c78564334c5daad3e
Michael Gerhaeuser <michael.gerhaeuser@gmail.com>
2020-01-26 15:04:34 +0100

    Check for MongoDB availability on Docker start
    If no MongoDB is available, the container will output a warning
    with a pointer to our admin guide where we explain how to set up
    a MongoDB.

--- .deploy/4minitz.sh
+++ .deploy/4minitz.sh
@@ -31,6 +31,76 @@ export METEOR_SETTINGS=$(cat ${basedir4min}/4minitz_settings.json)
 export UNIVERSE_I18N_LOCALES='all'
 
 echo ""
+echo "Checking for MongoDB"
+
+# From https://gist.github.com/megahertz/c46136a77c4d42e285ec5a2f32215903
+#
+# Usage: parse_mongo_url URL
+# It sets the following variables:
+# MONGO_HOST
+# MONGO_PORT
+# MONGO_DATABASE
+# MONGO_USER
+# MONGO_PASSWORD
+function parse_mongo_url {
+    local MONGO_URL="$1"
+
+    local AUTH
+    local HOST_PORT
+    local PROTO
+    local URL
+
+    PROTO="$(echo ${MONGO_URL} | grep :// | sed -e's,^\(.*://\).*,\1,g')"
+    URL="${MONGO_URL/${PROTO}/}"
+
+    AUTH="$(echo ${URL} | grep @ | rev | cut -d@ -f2- | rev)"
+    if [ -n ${AUTH} ]; then
+        URL="$(echo ${URL} | rev | cut -d@ -f1 | rev)"
+    fi
+
+    MONGO_PASSWORD="$(echo ${AUTH} | grep : | cut -d: -f2-)"
+    if [ -n "${MONGO_PASSWORD}" ]; then
+        MONGO_USER="$(echo ${AUTH} | grep : | cut -d: -f1)"
+    else
+        MONGO_USER=${AUTH}
+    fi
+
+    HOST_PORT="$(echo ${URL} | cut -d/ -f1)"
+    MONGO_PORT="$(echo ${HOST_PORT} | grep : | cut -d: -f2)"
+    if [ -n "${MONGO_PORT}" ]; then
+        MONGO_HOST="$(echo ${HOST_PORT} | grep : | cut -d: -f1)"
+    else
+        MONGO_PORT=27017
+        MONGO_HOST="${HOST_PORT}"
+    fi
+
+    MONGO_DATABASE="$(echo ${URL} | grep / | cut -d/ -f2-)"
+}
+
+parse_mongo_url ${MONGO_URL}
+
+# Wait until mongo logs that it's ready (or timeout after 60s)
+COUNTER=0
+while !(nc -z ${MONGO_HOST} ${MONGO_PORT}) && [[ $COUNTER -lt 60 ]] ; do
+    sleep 2
+    let COUNTER+=2
+    echo "Waiting for mongo to initialize... ($COUNTER seconds so far)"
+done
+
+if [[ $COUNTER -ge 60 ]]; then
+    echo "MongoDB not found! Aborting 4Minitz start."
+    echo "Did you set up a MongoDB and pass in the correct MONGO_URL?"
+    echo "Since version 2.0 4Minitz longer comes bundled with a MongoDB."
+    echo "You need to set up your own database instance. See our admin guide:"
+    echo ""
+    echo "https://github.com/4minitz/4minitz/blob/develop/doc/admin/adminguide.md"
+    echo ""
+    exit 1
+fi
+
+echo "MongoDB is up and running, starting 4Minitz..."
+
+echo ""
 echo "***********************************"
 echo "*-- Welcome to 4Minitz in Docker--*"
 echo "*                                 *"
