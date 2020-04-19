cdd726ff0421df878f9e9200f46baf532970d197
Wolfram Esser <github@derwok.de>
2020-04-19 18:33:36 +0200

    e2e screensnapshots

--- tests/e2e2/helloWDIO-test.js
+++ tests/e2e2/helloWDIO-test.js
@@ -3,5 +3,9 @@ describe('suite sync', () => {
         () => {
             browser.url('http://localhost:3100');
             expect(browser.getTitle()).to.contain('4Minitz');
+
+            let dateStr = (new Date()).toISOString().replace(/[^0-9]/g, '') + '_';
+            let fullpath = './tests/snapshots/' + dateStr + '.png';
+            browser.saveScreenshot(fullpath);
         });
 });
