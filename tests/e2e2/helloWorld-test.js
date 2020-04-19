e8737982d2fc55979d6447447b7f2b739e7c6b68
Michael Gerhäuser <michael.gerhaeuser@methodpark.de>
2020-04-19 17:01:22 +0200

    Add Server helper

--- tests/e2e2/helloWorld-test.js
+++ tests/e2e2/helloWorld-test.js
@@ -1,7 +1,14 @@
+require('../end2end/helpers/Server');
+
 describe('suite sync', () => {
-    it('test sync',
-        () => {
-            browser.url('https://webdriver.io');
-            expect(browser.getTitle()).to.equal('WebdriverIO · Next-gen browser automation test framework for Node.js');
-        });
+    before('reset app', () => {
+        server.connect();
+        server.call('e2e.resetMyApp', false);
+        server.close();
+    });
+
+    it('test sync', () => {
+        browser.url('https://webdriver.io');
+        expect(browser.getTitle()).to.equal('WebdriverIO · Next-gen browser automation test framework for Node.js');
+    });
 });
