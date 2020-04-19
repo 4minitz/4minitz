44ad1394c5753f697594bb898f2db12ca6d6740d
Wolfram Esser <github@derwok.de>
2020-04-19 18:17:47 +0200

    wip e2e

--- tests/e2e2/helloWDIO-test.js
+++ tests/e2e2/helloWDIO-test.js
@@ -2,6 +2,6 @@ describe('suite sync', () => {
     it('test sync',
         () => {
             browser.url('http://localhost:3100');
-            expect(browser.getTitle()).to.contain('4Minitz XX');
+            expect(browser.getTitle()).to.contain('4Minitz');
         });
 });
