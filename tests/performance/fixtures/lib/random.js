0e7e281def499b889889a4b943ceaa640a2c9169
Restyled.io <commits@restyled.io>
2023-09-28 05:57:55 +0000

    Restyled by clang-format

--- tests/performance/fixtures/lib/random.js
+++ tests/performance/fixtures/lib/random.js
@@ -1,15 +1,17 @@
 var randomstring = require("randomstring");
-const { faker } = require("@faker-js/faker");
+const {faker} = require("@faker-js/faker");
 
 export class Random {
   static generateId() {
     // unique id from the random package also used by minimongo
-    // character list: https://github.com/meteor/meteor/blob/release/METEOR%401.4.0.1/packages/random/random.js#L88
-    // string length: https://github.com/meteor/meteor/blob/release/METEOR%401.4.0.1/packages/random/random.js#L197
+    // character list:
+    // https://github.com/meteor/meteor/blob/release/METEOR%401.4.0.1/packages/random/random.js#L88
+    // string length:
+    // https://github.com/meteor/meteor/blob/release/METEOR%401.4.0.1/packages/random/random.js#L197
     return randomstring.generate({
-      length: 17,
-      readable: true,
-      charset: "alphanumeric",
+      length : 17,
+      readable : true,
+      charset : "alphanumeric",
     });
   }
 
@@ -19,8 +21,8 @@ export class Random {
 
   static generateMeetingSeriesValues() {
     return {
-      project: faker.company.name(),
-      name: faker.person.fullName(),
+      project : faker.company.name(),
+      name : faker.person.fullName(),
     };
   }
 }
