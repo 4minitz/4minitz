aa475f529c1d54ffb8f2ef8f972cd956816c99f7
Ciarán <bubonicfred@gmail.com>
2023-09-28 10:48:12 +1000

    style: style class

--- tests/performance/fixtures/lib/random.js
+++ tests/performance/fixtures/lib/random.js
@@ -1,28 +1,26 @@
-const  { randomstring } = require('randomstring')
-const { faker } = require('@faker-js/faker');
+const { randomstring } = require("randomstring");
+const { faker } = require("@faker-js/faker");
 
 export class Random {
+  static generateId() {
+    // unique id from the random package also used by minimongo
+    // character list: https://github.com/meteor/meteor/blob/release/METEOR%401.4.0.1/packages/random/random.js#L88
+    // string length: https://github.com/meteor/meteor/blob/release/METEOR%401.4.0.1/packages/random/random.js#L197
+    return randomstring.generate({
+      length: 17,
+      readable: true,
+      charset: "alphanumeric",
+    });
+  }
 
-    static generateId() {
-        // unique id from the random package also used by minimongo
-        // character list: https://github.com/meteor/meteor/blob/release/METEOR%401.4.0.1/packages/random/random.js#L88
-        // string length: https://github.com/meteor/meteor/blob/release/METEOR%401.4.0.1/packages/random/random.js#L197
-        return randomstring.generate({
-            length: 17,
-            readable: true,
-            charset: 'alphanumeric'
-          });
+  static randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
+  static generateMeetingSeriesValues() {
+    return {
+      project: faker.company.companyName(),
+      name: faker.name.findName(),
     };
-
-    static randomNumber(min, max) {
-        return Math.floor(Math.random()*(max-min+1)+min);
-    }
-
-    static generateMeetingSeriesValues() {
-        return {
-            project: faker.company.companyName(),
-            name: faker.name.findName()
-        }
-    }
-
+  }
 }
