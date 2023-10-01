e3b8a41d2082f8da6193e0ad3625404bee2a456a
Restyled.io <commits@restyled.io>
2023-10-01 03:55:00 +0000

    Restyled by shellharden

--- .deploy/4minitz.sh
+++ .deploy/4minitz.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 basedir4min=/4minitz_storage
-settingsfile=${basedir4min}/4minitz_settings.json
+settingsfile=$basedir4min/4minitz_settings.json
 
 if [[ ! -d $basedir4min ]]
 then
@@ -17,7 +17,7 @@ then
     echo "4minitz_settings.json found on your local host directory."
 else
     echo "Copying 4minitz_settings.json to your local host directory - once!"
-    cp /4minitz_settings.json ${settingsfile}
+    cp /4minitz_settings.json "$settingsfile"
     echo "DONE."
 fi
 echo "You may edit the settings file locally on your host."
@@ -27,7 +27,7 @@ cd /4minitz_bin/bundle || exit
 
 export PORT=3333
 export ROOT_URL='http://localhost:3100'
-export METEOR_SETTINGS=$(cat ${basedir4min}/4minitz_settings.json)
+export METEOR_SETTINGS="$(cat "$basedir4min"/4minitz_settings.json)"
 export UNIVERSE_I18N_LOCALES='all'
 
 echo ""
@@ -50,38 +50,38 @@ function parse_mongo_url {
     local PROTO
     local URL
 
-    PROTO="$(echo "${MONGO_URL}" | grep :// | sed -e's,^\(.*://\).*,\1,g')"
+    PROTO="$(echo "$MONGO_URL" | grep :// | sed -e's,^\(.*://\).*,\1,g')"
     URL="${MONGO_URL/${PROTO}/}"
 
-    AUTH="$(echo "${URL}" | grep @ | rev | cut -d@ -f2- | rev)"
+    AUTH="$(echo "$URL" | grep @ | rev | cut -d@ -f2- | rev)"
     if [[ -n ${AUTH} ]]; then
-        URL="$(echo "${URL}" | rev | cut -d@ -f1 | rev)"
+        URL="$(echo "$URL" | rev | cut -d@ -f1 | rev)"
     fi
 
-    MONGO_PASSWORD="$(echo "${AUTH}" | grep : | cut -d: -f2-)"
-    if [ -n "${MONGO_PASSWORD}" ]; then
-        MONGO_USER="$(echo "${AUTH}" | grep : | cut -d: -f1)"
+    MONGO_PASSWORD="$(echo "$AUTH" | grep : | cut -d: -f2-)"
+    if [ -n "$MONGO_PASSWORD" ]; then
+        MONGO_USER="$(echo "$AUTH" | grep : | cut -d: -f1)"
     else
-        MONGO_USER=${AUTH}
+        MONGO_USER=$AUTH
     fi
 
-    HOST_PORT="$(echo "${URL}" | cut -d/ -f1)"
-    MONGO_PORT="$(echo "${HOST_PORT}" | grep : | cut -d: -f2)"
-    if [ -n "${MONGO_PORT}" ]; then
-        MONGO_HOST="$(echo "${HOST_PORT}" | grep : | cut -d: -f1)"
+    HOST_PORT="$(echo "$URL" | cut -d/ -f1)"
+    MONGO_PORT="$(echo "$HOST_PORT" | grep : | cut -d: -f2)"
+    if [ -n "$MONGO_PORT" ]; then
+        MONGO_HOST="$(echo "$HOST_PORT" | grep : | cut -d: -f1)"
     else
         MONGO_PORT=27017
-        MONGO_HOST="${HOST_PORT}"
+        MONGO_HOST="$HOST_PORT"
     fi
 
-    MONGO_DATABASE="$(echo "${URL}" | grep / | cut -d/ -f2-)"
+    MONGO_DATABASE="$(echo "$URL" | grep / | cut -d/ -f2-)"
 }
 
-parse_mongo_url "${MONGO_URL}"
+parse_mongo_url "$MONGO_URL"
 
 # Wait until mongo logs that it's ready (or timeout after 60s)
 COUNTER=0
-while ! (nc -z "${MONGO_HOST}" "${MONGO_PORT}") && [[ $COUNTER -lt 60 ]] ; do
+while ! (nc -z "$MONGO_HOST" "$MONGO_PORT") && [[ $COUNTER -lt 60 ]] ; do
     sleep 2
     let COUNTER+=2
     echo "Waiting for mongo to initialize... ($COUNTER seconds so far)"
