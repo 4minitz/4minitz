be31fe9c9b2ba02e18219e003a62678d9cef639d
Restyled.io <commits@restyled.io>
2023-10-03 07:57:40 +0000

    Restyled by prettier

--- client/templates/meetingseries/tabTopics.js
+++ client/templates/meetingseries/tabTopics.js
@@ -1,12 +1,12 @@
-import {TOPIC_KEYWORDS} from "/imports/search/FilterKeywords";
-import {QueryParser} from "/imports/search/QueryParser";
-import {TopicsFilter} from "/imports/search/TopicsFilter";
-import {ReactiveVar} from "meteor/reactive-var";
-import {Template} from "meteor/templating";
-import {i18n} from "meteor/universe:i18n";
+import { TOPIC_KEYWORDS } from "/imports/search/FilterKeywords";
+import { QueryParser } from "/imports/search/QueryParser";
+import { TopicsFilter } from "/imports/search/TopicsFilter";
+import { ReactiveVar } from "meteor/reactive-var";
+import { Template } from "meteor/templating";
+import { i18n } from "meteor/universe:i18n";
 
-import {FilterControlConfig} from "../globals/ui-controls/filterControl";
-import {TopicListConfig} from "../topic/topicsList";
+import { FilterControlConfig } from "../globals/ui-controls/filterControl";
+import { TopicListConfig } from "../topic/topicsList";
 
 import {
   createLabelIdsReceiver,
@@ -20,35 +20,36 @@ export class TabTopicsConfig {
   }
 }
 
-Template.tabTopics.onCreated(function() {
+Template.tabTopics.onCreated(function () {
   this.topicFilterQuery = new ReactiveVar("");
   let myTemplate = Template.instance();
-  this.topicFilterHandler =
-      (query) => { myTemplate.topicFilterQuery.set(query); };
+  this.topicFilterHandler = (query) => {
+    myTemplate.topicFilterQuery.set(query);
+  };
   this.topicFilter = new TopicsFilter();
   this.parser = new QueryParser(
-      TOPIC_KEYWORDS,
-      createLabelIdsReceiver(myTemplate.data.parentMeetingSeriesId),
-      createUserIdsReceiver,
+    TOPIC_KEYWORDS,
+    createLabelIdsReceiver(myTemplate.data.parentMeetingSeriesId),
+    createUserIdsReceiver,
   );
 });
 
 Template.tabTopics.helpers({
-  getTopicFilterConfig : function() {
+  getTopicFilterConfig: function () {
     const FILTERS = [
-      {text : i18n.__("Topic.Filter.uncompleted"), value : "is:uncompleted"},
-      {text : i18n.__("Topic.Filter.completed"), value : "is:completed"},
-      {text : i18n.__("Topic.Filter.yourTopic"), value : "@me"},
+      { text: i18n.__("Topic.Filter.uncompleted"), value: "is:uncompleted" },
+      { text: i18n.__("Topic.Filter.completed"), value: "is:completed" },
+      { text: i18n.__("Topic.Filter.yourTopic"), value: "@me" },
     ];
     return new FilterControlConfig(
-        Template.instance().topicFilterHandler,
-        FILTERS,
-        TOPIC_KEYWORDS,
-        "Topic-Filter",
+      Template.instance().topicFilterHandler,
+      FILTERS,
+      TOPIC_KEYWORDS,
+      "Topic-Filter",
     );
   },
 
-  topicViewData : function() {
+  topicViewData: function () {
     let tmpl = Template.instance();
     let query = tmpl.topicFilterQuery.get();
     tmpl.parser.reset();
@@ -56,10 +57,10 @@ Template.tabTopics.helpers({
 
     let topics = tmpl.topicFilter.filter(tmpl.data.topics, tmpl.parser);
     return new TopicListConfig(
-        topics,
-        null,
-        true,
-        tmpl.data.parentMeetingSeriesId,
+      topics,
+      null,
+      true,
+      tmpl.data.parentMeetingSeriesId,
     );
   },
 });
